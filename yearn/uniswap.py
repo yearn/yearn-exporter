7766142be66eea6521d9b67330c8847152391418
banteg <banteeg@gmail.com>
2020-09-22 20:40:04 +0200

    add wbtc to uniswap

--- yearn/uniswap.py
+++ yearn/uniswap.py
@@ -5,6 +5,7 @@ router = interface.UniswapRouter('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
 weth = interface.ERC20('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 usdc = interface.ERC20('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
 dai = interface.ERC20('0x6B175474E89094C44Da98b954EedeAC495271d0F')
+wbtc = interface.ERC20('0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599')
 
 
 def price_router(token_in, token_out):
