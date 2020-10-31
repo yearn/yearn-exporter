9adc45714b2e7b83f8b768309153324e8859479d
banteg <banteeg@gmail.com>
2020-10-31 16:03:04 +0300

    feat: separate base description and strategy desc

--- yearn/strategies_v2.py
+++ yearn/strategies_v2.py
@@ -1,17 +1,19 @@
+from abc import abstractmethod
 from dataclasses import dataclass
 from brownie import interface
 from brownie.network.contract import InterfaceContainer
 
 
-@dataclass
 class BaseStrategy:
-    strategy: InterfaceContainer
-    vault: InterfaceContainer = None
+    interface = None
 
-    def __post_init__(self):
-        self.vault = interface.Vault(self.strategy.vault())
+    def __init__(self, strategy, strategy_interface=None):
+        if strategy_interface:
+            self.interface = strategy_interface
+        self.strategy = self.interface(strategy)
+        self.vault = interface.Vault(strategy.vault())
 
-    def describe(self):
+    def describe_base(self):
         scale = 10 ** self.vault.decimals()
         params = self.vault.strategies(self.strategy)
         return {
@@ -30,4 +32,23 @@ class BaseStrategy:
             "lastReport": params[4],
             "totalDebt": params[5] / scale,
             "totalReturns": params[6] / scale,
+        }
+
+    def describe_strategy(self):
+        return {}
+
+    def describe(self):
+        return {
+            **self.describe_base(),
+            **self.describe_strategy(),
+        }
+
+
+@dataclass
+class StrategyUniswapPair(BaseStrategy):
+    interface = interface.StrategyUniswapPair
+
+    def describe_strategy(self):
+        return {
+            "wantPrice": self.strategy.wantPrice().to("ether"),
         }
