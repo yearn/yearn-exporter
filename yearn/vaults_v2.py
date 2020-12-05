4fa754b00968026f35a8c1f3dd954364a738efb9
banteg <banteeg@gmail.com>
2020-12-06 01:32:52 +0300

    rm 0.1.x vaults

--- yearn/vaults_v2.py
+++ yearn/vaults_v2.py
@@ -55,43 +55,16 @@ class VaultV2:
 
 
 VAULTS = [
-    VaultV2(
-        name="CRV",
-        vault=interface.Vault("0x2832817633520BF0da1b57E2Fb7bb2Fba95014F9"),
-        strategies=[
-            strategies.Strategy("0xACb5eBaa9bAc72581b269077Ef4D0BA3Eefac2b7", interface.StrategyCreamCRV)
-        ],
-    ),
-    VaultV2(
-        name="WETH",
-        vault=interface.Vault("0xF20731F26E98516dd83bb645DD757D33826a37b5"),
-        strategies=[
-            strategies.Strategy("0x97785a81B3505Ea9026b2aFFa709dfd0C9Ef24f6", interface.YearnWethCreamStratV2),
-        ],
-    ),
-    VaultV2(
-        name="HEGIC",
-        vault=interface.Vault("0xBe77B53a165d3109AE9500EbAA9328b577960aBF"),
-        strategies=[
-            strategies.Strategy("0x4141b5e7b687a89D994Bff1b35C8082404cA70A7", interface.StrategyHegic),
-            strategies.Strategy("0xaE466D9Cb123E26b369C63ba89F8588be5f19E5b", interface.StrategyHegicWBTC),
-        ],
-    ),
-    VaultV2(
-        name="Uniswap DAI/WETH",
-        vault=interface.Vault("0x7b9CB3CfEE697Cd1761945e8B42032c8CCe407DA"),
-        strategies=[strategies.StrategyUniswapPairPickle("0x1dc7aB3BBFAE5000FFDc4289E3D91717f3A9D898"),],
-    ),
+    # 0.2.2 DAI
     VaultV2(
         name="DAI",
-        vault=interface.Vault("0xBFa4D8AA6d8a379aBFe7793399D3DdaCC5bBECBB"),  # 0.2.2
-        strategies=[
-            strategies.Strategy("0x5A9D49679319FCF3AcFe5559602Dbf31A221BaD6", interface.GenericLevCompFarm),
-        ],
+        vault=interface.Vault("0xBFa4D8AA6d8a379aBFe7793399D3DdaCC5bBECBB"),
+        strategies=[strategies.Strategy("0x5A9D49679319FCF3AcFe5559602Dbf31A221BaD6", interface.GenericLevCompFarm),],
     ),
+    # 0.2.2 USDC
     VaultV2(
         name="USDC",
-        vault=interface.Vault("0xe2F6b9773BF3A015E2aA70741Bde1498bdB9425b"),  # 0.2.2
+        vault=interface.Vault("0xe2F6b9773BF3A015E2aA70741Bde1498bdB9425b"),
         strategies=[]
     ),
 ]
