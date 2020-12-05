3b3655413f1a0f972644b5386cc0ead60b781273
banteg <banteeg@gmail.com>
2020-12-06 01:47:42 +0300

    feat: refactor vaults v1

--- scripts/yearn.py
+++ scripts/yearn.py
@@ -1,72 +1,49 @@
-import re
 import warnings
-from dataclasses import dataclass
-from typing import Optional, Union
 
-from brownie import accounts, chain, interface, rpc, web3
-from brownie.network.contract import InterfaceContainer
+import toml
+from brownie import chain
 from click import secho
 from prometheus_client import Gauge, start_http_server
 
-from yearn import constants, curve, uniswap
-from yearn.vaults import Vault, load_registry, load_vaults
-from yearn import vaults_v2
-import toml
+from yearn import vaults_v1, vaults_v2
 
 warnings.simplefilter("ignore")
 
 
-def describe_vault(vault: Vault):
-    info = {
-        "vault balance": vault.vault.balance() / vault.scale,
-        "share price": vault.vault.getPricePerFullShare() / 1e18,
-        "vault total": vault.vault.totalSupply() / vault.scale,
-        "strategy balance": vault.strategy.balanceOf() / vault.scale,
-    }
-
-    # some of the oldest vaults don't implement these methods
-    if hasattr(vault.vault, "available"):
-        info["available"] = vault.vault.available() / vault.scale
-
-    if hasattr(vault.vault, "min") and hasattr(vault.vault, "max"):
-        info["strategy buffer"] = vault.vault.min() / vault.vault.max()
-
-    # new curve voter proxy vaults
-    if hasattr(vault.strategy, "proxy"):
-        vote_proxy = interface.CurveYCRVVoter(vault.strategy.voter())
-        swap_func = {"StrategyCurveGUSDProxy": "SWAP"}.get(vault.strategy._name, "curve")
-        swap = interface.CurveSwap(getattr(vault.strategy, swap_func)())
-        gauge = interface.CurveGauge(vault.strategy.gauge())
-        info.update(curve.calculate_boost(gauge, vote_proxy))
-        info.update(curve.calculate_apy(gauge, swap))
-        info["earned"] = gauge.claimable_tokens.call(vote_proxy).to("ether")
-
-    if hasattr(vault.strategy, "earned"):
-        info["lifetime earned"] = vault.strategy.earned() / vault.scale
-
-    if vault.strategy._name == "StrategyYFIGovernance":
-        ygov = interface.YearnGovernance(vault.strategy.gov())
-        info["earned"] = ygov.earned(vault.strategy) / 1e18
-        info["reward rate"] = ygov.rewardRate() / 1e18
-        info["ygov balance"] = ygov.balanceOf(vault.strategy) / 1e18
-        info["ygov total"] = ygov.totalSupply() / 1e18
-        info["token price"] = uniswap.price_router(vault.token, uniswap.usdc)
-
-    return info
-
-
-def develop():
-    registry = load_registry()
-    vaults = load_vaults(registry)
+def develop_v1():
+    registry = vaults_v1.load_registry()
+    vaults = vaults_v1.load_vaults(registry)
     for i, vault in enumerate(vaults):
         secho(vault.name, fg="yellow")
         secho(str(vault), dim=True)
         try:
-            info = describe_vault(vault)
+            info = vault.describe()
             for a, b in info.items():
                 print(f"{a} = {b}")
         except ValueError as e:
-            print('error', e)
+            print("error", e)
+
+
+def exporter_v1():
+    prom_gauge = Gauge("yearn", "yearn stats", ["vault", "param"])
+    timing = Gauge("yearn_timing", "", ["vault", "action"])
+    start_http_server(8800)
+    registry = vaults_v1.load_registry()
+    # load vaults once, todo: update params
+    with timing.labels("registry", "load").time():
+        vaults = vaults_v1.load_vaults(registry)
+    for block in chain.new_blocks():
+        secho(f"{block.number}", fg="green")
+        for vault in vaults:
+            with timing.labels(vault.name, "describe").time():
+                try:
+                    info = vault.describe()
+                except ValueError as e:
+                    print("error", e)
+                    continue
+            for param, value in info.items():
+                # print(f'{param} = {value}')
+                prom_gauge.labels(vault.name, param).set(value)
 
 
 def develop_v2():
@@ -95,58 +72,3 @@ def exporter_v2():
             for strat in info["strategies"]:
                 for param, value in info["strategies"][strat].items():
                     strat_gauge.labels(vault.name, strat, param).set(value)
-
-
-def exporter():
-    prom_gauge = Gauge("yearn", "yearn stats", ["vault", "param"])
-    timing = Gauge("yearn_timing", "", ["vault", "action"])
-    start_http_server(8800)
-    registry = load_registry()
-    # load vaults once, todo: update params
-    with timing.labels("registry", "load").time():
-        vaults = load_vaults(registry)
-    for block in chain.new_blocks():
-        secho(f"{block.number}", fg="green")
-        for vault in vaults:
-            with timing.labels(vault.name, "describe").time():
-                try:
-                    info = describe_vault(vault)
-                except ValueError as e:
-                    print('error', e)
-                    continue
-            for param, value in info.items():
-                # print(f'{param} = {value}')
-                prom_gauge.labels(vault.name, param).set(value)
-
-
-def audit():
-    """
-    Audit vault and strategy configuration.
-    """
-    governance = web3.ens.resolve("ychad.eth")
-    registry = load_registry()
-    vaults = load_vaults(registry)
-    for v in vaults:
-        if v.vault.governance() != governance:
-            secho(f"{v.name} vault governance == {v.vault.governance()}", fg="red")
-            print(f"{v.vault}.setGovernance({governance})")
-        if v.strategy.governance() != governance:
-            secho(f"{v.name} strategy governance == {v.strategy.governance()}", fg="red")
-            print(f"{v.strategy}.setGovernance({governance})")
-
-
-def harvest():
-    assert rpc.is_active()
-    andre = accounts.at("andrecronje.eth", force=True)
-    print(andre)
-    governance = web3.ens.resolve("ychad.eth")
-    registry = load_registry()
-    vaults = load_vaults(registry)
-    for v in vaults:
-        secho(v.name, fg="green")
-        print(v)
-        try:
-            tx = v.strategy.harvest({"from": andre})
-            tx.info()
-        except AttributeError:
-            pass
---
+++ yearn/vaults_v1.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from brownie import interface, web3
+from brownie.network.contract import InterfaceContainer
+
+from yearn import constants, curve, uniswap
+
+
+@dataclass
+class VaultV1:
+    vault: InterfaceContainer
+    controller: InterfaceContainer
+    token: interface.ERC20
+    strategy: str
+    is_wrapped: bool
+    is_delegated: bool
+    # the rest is populated in post init
+    name: Optional[str] = None
+    decimals: Optional[int] = None
+
+    def __post_init__(self):
+        self.vault = constants.VAULT_INTERFACES.get(self.vault, interface.yVault)(self.vault)
+        self.controller = constants.CONTROLLER_INTERFACES[self.controller](self.controller)
+        self.strategy = constants.STRATEGY_INTERFACES[self.strategy](self.strategy)
+        self.token = interface.ERC20(self.token)
+        self.name = constants.VAULT_ALIASES[str(self.vault)]
+        self.decimals = self.vault.decimals()  # vaults inherit decimals from token
+
+    def describe(self):
+        scale = 10 ** self.decimals
+        info = {
+            "vault balance": self.vault.balance() / scale,
+            "share price": self.vault.getPricePerFullShare() / 1e18,
+            "vault total": self.vault.totalSupply() / scale,
+            "strategy balance": self.strategy.balanceOf() / scale,
+        }
+
+        # some of the oldest vaults don't implement these methods
+        if hasattr(self.vault, "available"):
+            info["available"] = self.vault.available() / scale
+
+        if hasattr(self.vault, "min") and hasattr(self.vault, "max"):
+            info["strategy buffer"] = self.vault.min() / self.vault.max()
+
+        # new curve voter proxy vaults
+        if hasattr(self.strategy, "proxy"):
+            vote_proxy = interface.CurveYCRVVoter(self.strategy.voter())
+            swap_func = {"StrategyCurveGUSDProxy": "SWAP"}.get(self.strategy._name, "curve")
+            swap = interface.CurveSwap(getattr(self.strategy, swap_func)())
+            gauge = interface.CurveGauge(self.strategy.gauge())
+            info.update(curve.calculate_boost(gauge, vote_proxy))
+            info.update(curve.calculate_apy(gauge, swap))
+            info["earned"] = gauge.claimable_tokens.call(vote_proxy).to("ether")
+
+        if hasattr(self.strategy, "earned"):
+            info["lifetime earned"] = self.strategy.earned() / scale
+
+        if self.strategy._name == "StrategyYFIGovernance":
+            ygov = interface.YearnGovernance(self.strategy.gov())
+            info["earned"] = ygov.earned(self.strategy) / 1e18
+            info["reward rate"] = ygov.rewardRate() / 1e18
+            info["ygov balance"] = ygov.balanceOf(self.strategy) / 1e18
+            info["ygov total"] = ygov.totalSupply() / 1e18
+            info["token price"] = uniswap.price_router(self.token, uniswap.usdc)
+
+        return info
+
+
+def load_registry(address="registry.ychad.eth"):
+    return interface.YRegistry(web3.ens.resolve(address))
+
+
+def load_vaults(registry):
+    return [VaultV1(*params) for params in zip(registry.getVaults(), *registry.getVaultsInfo())]
