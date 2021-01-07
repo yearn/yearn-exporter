7254e995d6999c921e9f32d63460cd7a3304182c
banteg <banteeg@gmail.com>
2021-01-07 07:43:24 +0300

    feat: shorcut for stablecoin price, uni lp price

--- yearn/uniswap.py
+++ yearn/uniswap.py
@@ -1,24 +1,39 @@
 from brownie import interface
 
-factory = interface.UniswapFactory('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
-router = interface.UniswapRouter('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
-weth = interface.ERC20('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
-usdc = interface.ERC20('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
-dai = interface.ERC20('0x6B175474E89094C44Da98b954EedeAC495271d0F')
-wbtc = interface.ERC20('0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599')
+factory = interface.UniswapFactory("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")
+
+FACTORIES = {
+    "uniswap": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    "sushiswap": "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac",
+}
+ROUTERS = {
+    "uniswap": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "sushiswap": "0xD9E1CE17F2641F24AE83637AB66A2CCA9C378B9F",
+}
+FACTORY_TO_ROUTER = {FACTORIES[name]: ROUTERS[name] for name in FACTORIES}
+STABLECOINS = {
+    "usdc": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    "tusd": "0x0000000000085d4780B73119b644AE5ecd22b376",
+    "dai": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    "usdt": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+}
+weth = interface.ERC20("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
+usdc = interface.ERC20("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
+dai = interface.ERC20("0x6B175474E89094C44Da98b954EedeAC495271d0F")
+wbtc = interface.ERC20("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599")
 
 
-def price_router(token_in, token_out):
+def price_router(token_in, token_out=usdc, router=None):
     """
     Calculate a price based on Uniswap Router quote for selling one `token_in`.
     Always uses intermediate WETH pair.
     """
-    if token_in == token_out:
-        return 1
-    amount_in = 10 ** interface.ERC20(token_in).decimals()
+    tokens = [interface.ERC20(token) for token in [token_in, token_out]]
+    router = interface.UniswapRouter(router or ROUTERS["uniswap"])
+    amount_in = 10 ** tokens[0].decimals()
     path = [token_in, token_out] if weth in (token_in, token_out) else [token_in, weth, token_out]
     quote = router.getAmountsOut(amount_in, path)
-    amount_out = quote[-1] / 10 ** interface.ERC20(token_out).decimals()
+    amount_out = quote[-1] / 10 ** tokens[1].decimals()
     return amount_out
 
 
@@ -28,7 +43,34 @@ def price_direct(token_in, token_out):
     Only supports direct pairs.
     """
     pair = interface.UniswapPair(factory.getPair(token_in, token_out))
-    reserves = dict(zip([pair.token0(), pair.token1()], pair.getReserves()[:2]))
+    reserves = dict(zip([pair.token0(), pair.token1()], pair.getReserves()))
     reserves_in = reserves[token_in] / 10 ** interface.ERC20(token_in).decimals()
     reserves_out = reserves[token_out] / 10 ** interface.ERC20(token_out).decimals()
     return reserves_out / reserves_in
+
+
+def is_uniswap_pool(address):
+    try:
+        return interface.UniswapPair(address).factory() in FACTORY_TO_ROUTER
+    except (ValueError, OverflowError):
+        pass
+    return False
+
+
+def uniswap_lp_price(address):
+    """ Get Uniswap LP token price. """
+    pair = interface.UniswapPair(address)
+    router = FACTORY_TO_ROUTER[pair.factory()]
+    tokens = [interface.ERC20(token) for token in [pair.token0(), pair.token1()]]
+    scales = [10 ** token.decimals() for token in tokens]
+    prices = [price_router(token, router=router) for token in tokens]
+    supply = pair.totalSupply() / 1e18
+    return sum(reserve / scale * price for reserve, scale, price in zip(pair.getReserves(), scales, prices)) / supply
+
+
+def token_price(token):
+    if token in STABLECOINS:
+        return 1
+    if is_uniswap_pool(token):
+        return uniswap_lp_price(token)
+    return price_router(token)
