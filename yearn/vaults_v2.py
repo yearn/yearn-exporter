ebbd3802d1b3b8e3ee4e13a54011cf6c2393724d
Rudy Alvarez <rudygt@gmail.com>
2021-02-13 16:34:56 +0100

    fetch v2 vault list from the registry

--- yearn/vaults_v2.py
+++ yearn/vaults_v2.py
@@ -1,17 +1,22 @@
 from dataclasses import dataclass
 from typing import List
 
-from brownie import interface
+from brownie import interface, web3
 from brownie.network.contract import InterfaceContainer
 from packaging import version
+
+from eth_event import get_log_topic,get_topic_map,decode_logs
 
 from yearn import strategies
 from yearn import uniswap
 
+VAULTS_EVENT_TOPIC = '0xce089905ba4a4d622553bcb5646fd23e895c256f0376eee04e99e61cec1dc7e8'
+EXPERIMENTAL_VAULTS_EVENT_TOPIC = '0x57a9cdc2a05e05f66e76769bdbe88e21ec45d9ee0f97d4cb60395d4c75dcbcda'
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 MIN_VERSION = version.parse("0.2.0")
 
+REGISTRY_DEPLOYMENT_BLOCK = 11563389
 
 @dataclass
 class VaultV2:
@@ -50,7 +55,11 @@ class VaultV2:
         for strat in self.strategies:
             info["strategies"][strat.name] = strat.describe()
 
-        info["token price"] = uniswap.token_price(self.vault.token())
+        try:
+            info["token price"] = uniswap.token_price(self.vault.token())
+        except ValueError:
+            info["token price"] = 0
+
         if "totalAssets" in info:
             info["tvl"] = info["token price"] * info["totalAssets"]
 
@@ -65,7 +74,49 @@ vaults = {
     "WBTC 0.3.0": "0xcB550A6D4C8e3517A939BC79d0c7093eb7cF56B5",
 }
 
+def get_vault_addresses_from_events(topic):
+    registry = web3.ens.resolve("v2.registry.ychad.eth")
+
+    registry_v2 = interface.YRegistry2(registry)
+
+    topic_map = get_topic_map(registry_v2.abi)
+
+    event_filter = web3.eth.filter(
+        {'fromBlock': REGISTRY_DEPLOYMENT_BLOCK, 'toBlock': 'latest', 'address': registry, 'topics': [
+            topic]})
+
+    event_logs = web3.eth.getFilterLogs(event_filter.filter_id)
+
+    decoded = decode_logs(event_logs, topic_map)
+
+    vaults = []
+
+    for item in decoded:
+        vault = {}
+        for property in item['data']:
+            vault[property['name']] = property['value']
+
+        vaults.append(vault)
+
+    vaults_flat = [v['vault'] for v in vaults]
+
+    print("v2 vault addresses", vaults_flat)
+
+    return vaults_flat
 
 def get_vaults():
-    # TODO: read from registry
-    return [VaultV2(name=name, vault=interface.Vault(vault), strategies=[]) for name, vault in vaults.items()]
+
+    vaults_flat = get_vault_addresses_from_events(VAULTS_EVENT_TOPIC)
+
+    v2_vaults = []
+
+    # fetch vault name and api version to construct the description
+    for vault_address in vaults_flat:
+        contract = interface.Vault(vault_address)
+        name = contract.name()
+        api_version = contract.apiVersion()
+        description = name + ' ' + api_version
+        vault_object = VaultV2(name=description, vault=contract, strategies=[])
+        v2_vaults.append(vault_object)
+
+    return v2_vaults
