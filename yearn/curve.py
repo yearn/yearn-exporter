d4748b9056c058769c7c8f267a990f7c094a79d3
banteg <banteeg@gmail.com>
2021-02-18 02:44:48 +0300

    feat: multicall curve, cache coins, gauge controller, eth_getCode

--- yearn/curve.py
+++ yearn/curve.py
@@ -2,30 +2,38 @@ from brownie import interface
 
 from yearn import constants
 from yearn import uniswap
+from yearn.mutlicall import fetch_multicall
 
 
 crv = interface.ERC20("0xD533a949740bb3306d119CC777fa900bA034cd52")
 voting_escrow = interface.CurveVotingEscrow("0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2")
+gauge_controller = interface.CurveGaugeController("0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB")
 registry = interface.CurveRegistry("0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c")
+underlying_coins = {}
 
 
 def get_base_price(pool):
-    coins = set(registry.get_underlying_coins(pool))
-    if coins & constants.BTC_LIKE:
+    if pool not in underlying_coins:
+        underlying_coins[pool] = set(registry.get_underlying_coins(pool))
+    if underlying_coins[pool] & constants.BTC_LIKE:
         return uniswap.price_router(uniswap.wbtc, uniswap.usdc)
-    elif coins & constants.ETH_LIKE:
+    elif underlying_coins[pool] & constants.ETH_LIKE:
         return uniswap.price_router(uniswap.weth, uniswap.usdc)
     else:
         return 1
 
 
 def calculate_boost(gauge, addr):
-    gauge_balance = gauge.balanceOf(addr) / 1e18
-    gauge_total = gauge.totalSupply() / 1e18
-    working_balance = gauge.working_balances(addr) / 1e18
-    working_supply = gauge.working_supply() / 1e18
-    vecrv_balance = voting_escrow.balanceOf(addr) / 1e18
-    vecrv_total = voting_escrow.totalSupply() / 1e18
+    results = fetch_multicall(
+        [gauge, "balanceOf", addr],
+        [gauge, "totalSupply"],
+        [gauge, "working_balances", addr],
+        [gauge, "working_supply"],
+        [voting_escrow, "balanceOf", addr],
+        [voting_escrow, "totalSupply"],
+    )
+    results = [x / 1e18 for x in results]
+    gauge_balance, gauge_total, working_balance, working_supply, vecrv_balance, vecrv_total = results
     try:
         boost = working_balance / gauge_balance * 2.5
     except ZeroDivisionError:
@@ -58,11 +66,14 @@ def calculate_boost(gauge, addr):
 
 def calculate_apy(gauge, swap):
     crv_price = uniswap.price_router(crv, uniswap.usdc)
-    gauge_controller = interface.CurveGaugeController(gauge.controller())
-    working_supply = gauge.working_supply() / 1e18
-    relative_weight = gauge_controller.gauge_relative_weight(gauge) / 1e18
-    inflation_rate = gauge.inflation_rate() / 1e18
-    virtual_price = swap.get_virtual_price() / 1e18
+    results = fetch_multicall(
+        [gauge, "working_supply"],
+        [gauge_controller, "gauge_relative_weight", gauge],
+        [gauge, "inflation_rate"],
+        [swap, "get_virtual_price"],
+    )
+    results = [x / 1e18 for x in results]
+    working_supply, relative_weight, inflation_rate, virtual_price = results
     base_price = get_base_price(swap)
     try:
         rate = (inflation_rate * relative_weight * 86400 * 365 / working_supply * 0.4) / (virtual_price * base_price)
--- yearn/instrumentation.py
+++ yearn/instrumentation.py
@@ -1,18 +1,24 @@
 from collections import Counter
 from tabulate import tabulate
 from brownie import web3
+from web3 import middleware
 
 stats = Counter()
 
+
 def request_stats(make_request, w3):
     def middleware(method, params):
+        print(method, params)
         stats[method] += 1
         response = make_request(method, params)
         return response
+
     return middleware
 
 
 def display():
-    print(tabulate(stats.most_common(), headers=['method', 'count']))
+    print(tabulate(stats.most_common(), headers=["method", "count"]))
 
-web3.middleware_onion.add(request_stats, 'requests_stats')
+
+web3.middleware_onion.add(middleware.construct_simple_cache_middleware(dict, ["eth_getCode"]))
+web3.middleware_onion.add(request_stats, "requests_stats")
