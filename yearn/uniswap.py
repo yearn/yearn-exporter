723733124059bf3e76375c93023566a292395396
banteg <banteeg@gmail.com>
2021-02-22 02:36:48 +0300

    fix: stablecoin price speedup

--- yearn/uniswap.py
+++ yearn/uniswap.py
@@ -11,11 +11,14 @@ ROUTERS = {
     "sushiswap": "0xD9E1CE17F2641F24AE83637AB66A2CCA9C378B9F",
 }
 FACTORY_TO_ROUTER = {FACTORIES[name]: ROUTERS[name] for name in FACTORIES}
+# these tokens are assumed to be priced at 1, the price is not queried
 STABLECOINS = {
     "usdc": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     "tusd": "0x0000000000085d4780B73119b644AE5ecd22b376",
     "dai": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     "usdt": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    "busd": "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
+    "susd": "0x57Ab1ec28D129707052df4dF418D58a2D46d5f51",
 }
 weth = interface.ERC20("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 usdc = interface.ERC20("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
@@ -69,7 +72,7 @@ def uniswap_lp_price(address):
 
 
 def token_price(token):
-    if token in STABLECOINS:
+    if token in STABLECOINS.values():
         return 1
     # stETH Curve LP
     if token == "0x06325440D014e39736583c165C2963BA99fAf14E":
