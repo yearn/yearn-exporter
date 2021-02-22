04ba0669abf4cdcb3deffd0a32dd1e4ffdf872fc
banteg <banteeg@gmail.com>
2021-02-22 04:46:22 +0300

    feat: explicitly enable instrumentation

--- yearn/instrumentation.py
+++ yearn/instrumentation.py
@@ -1,28 +1,27 @@
 from collections import Counter
-from tabulate import tabulate
+
+import click
 from brownie import web3
-from web3 import middleware
+from tabulate import tabulate
 
 stats = Counter()
-calls = Counter()
 
 
 def request_stats(make_request, w3):
     def middleware(method, params):
         stats[method] += 1
-        if method == 'eth_call':
-            print('.', end='', flush=True)
-            calls[params[0]['to']] += 1
+        if method == "eth_call":
+            print(".", end="", flush=True)
         response = make_request(method, params)
         return response
 
     return middleware
 
 
+def enable():
+    web3.middleware_onion.add(request_stats, "requests_stats")
+
+
 def display():
+    click.secho(f"{sum(stats.values())} calls", fg="yellow")
     print(tabulate(stats.most_common(), headers=["method", "count"]))
-    print(tabulate(calls.most_common(), headers=["call to", "count"]))
-
-
-web3.middleware_onion.add(middleware.construct_simple_cache_middleware(dict, ["eth_getCode"]))
-web3.middleware_onion.add(request_stats, "requests_stats")
