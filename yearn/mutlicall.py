ef4403b507513cb2a07beb1c965eeb2c6cb7d1c9
banteg <banteeg@gmail.com>
2021-03-17 14:46:01 +0300

    feat: chainlink batch cached price feed

--- yearn/mutlicall.py
+++ yearn/mutlicall.py
@@ -6,7 +6,7 @@ from brownie import Contract
 multicall = Contract("0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441")
 
 
-def fetch_multicall(*calls):
+def fetch_multicall(*calls, block=None):
     multicall_input = []
     fn_list = []
     decoded = []
@@ -22,7 +22,7 @@ def fetch_multicall(*calls):
 
         multicall_input.append((contract, fn.encode_input(*fn_inputs)))
 
-    response = multicall.aggregate.call(multicall_input)[1]
+    response = multicall.aggregate.call(multicall_input, block_identifier=block)[1]
 
     for fn, data in zip(fn_list, response):
         decoded.append(fn.decode_output(data.hex()))
@@ -30,11 +30,11 @@ def fetch_multicall(*calls):
     return decoded
 
 
-def multicall_matrix(contracts, params):
+def multicall_matrix(contracts, params, block=None):
     matrix = list(product(contracts, params))
     calls = [[contract, param] for contract, param in matrix]
 
-    results = fetch_multicall(*calls)
+    results = fetch_multicall(*calls, block_identifier=block)
 
     output = defaultdict(dict)
     for (contract, param), value in zip(matrix, results):
