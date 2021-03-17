317c0ee7141f0a6b6c29c336fe8f61dc3b43fa31
banteg <banteeg@gmail.com>
2021-03-18 02:22:42 +0300

    fix: disable brownie namespace warning

--- yearn/logs.py
+++ yearn/logs.py
@@ -1,4 +1,6 @@
 import logging
+import warnings
+from brownie.exceptions import BrownieEnvironmentWarning
 
 
 def setup_logging():
@@ -6,4 +8,10 @@ def setup_logging():
         level=logging.INFO,
         format="%(levelname)s %(name)s:%(lineno)d %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    warnings.filterwarnings(
+        'ignore',
+        r".*defines a 'balance' function.*",
+        BrownieEnvironmentWarning
     )
