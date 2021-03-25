213d037bb1a9769c57ee633ed09adc139a88a60b
banteg <banteeg@gmail.com>
2021-03-25 13:36:59 +0300

    feat: add ygov

--- yearn/special.py
+++ yearn/special.py
@@ -17,12 +17,28 @@ class Backscratcher:
         return crv_locked * crv_price
 
 
+class Ygov:
+    def __init__(self):
+        self.name = "yGov"
+        self.vault = Contract("0xBa37B002AbaFDd8E89a1995dA52740bbC013D992")
+        self.token = Contract("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e")
+
+    def total_value_at(self, block=None):
+        yfi_locked = self.token.balanceOf(self.vault, block_identifier=block) / 1e18
+        yfi_price = magic.get_price(str(self.token), block=block)
+        return yfi_locked * yfi_price
+
+
 class Registry:
     def __init__(self) -> None:
-        self.vaults = [Backscratcher()]
+        self.vaults = [
+            Backscratcher(),
+            Ygov(),
+        ]
 
     def describe(self, block=None):
-        return None
+        # not supported yet
+        return {}
 
     def total_value_at(self, block=None):
         vaults = self.active_vaults_at(block)
