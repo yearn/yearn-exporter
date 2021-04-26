a1d100d8e333036e2e9244a538f4ea31f35e6073
banteg <banteeg@gmail.com>
2021-04-26 20:25:46 +0300

    feat: simplify tvl script

--- scripts/tvl.py
+++ scripts/tvl.py
@@ -6,34 +6,18 @@ from brownie.exceptions import BrownieEnvironmentWarning
 from click import secho, style
 from toolz import groupby
 
-from yearn import iearn, ironbank, vaults_v1
-import yearn.v2.registry
+from yearn.yearn import Yearn
 
 warnings.simplefilter("ignore", BrownieEnvironmentWarning)
 
 
 def main():
     data = []
+    yearn = Yearn(load_strategies=False)
 
-    # iearn
-    iearn_registry = iearn.Registry()
-    for name, tvl in iearn_registry.total_value_at().items():
-        data.append({"product": "iearn", "name": name, "tvl": tvl})
-
-    # vaults v1
-    v1_registry = vaults_v1.Registry()
-    for name, tvl in v1_registry.total_value_at().items():
-        data.append({"product": "v1", "name": name, "tvl": tvl})
-
-    # vaults v2
-    v2_registry = yearn.v2.registry.Registry()
-    for name, tvl in v2_registry.total_value_at().items():
-        data.append({"product": "v2", "name": name, "tvl": tvl})
-
-    # ironbank
-    ib = ironbank.Registry()
-    for name, tvl in ib.total_value_at().items():
-        data.append({"product": "ib", "name": name, "tvl": tvl})
+    for product, registry in yearn.registries.items():
+        for name, tvl in registry.total_value_at().items():
+            data.append({"product": product, "name": name, "tvl": tvl})
 
     data.sort(key=lambda x: -x["tvl"])
     print(tabulate(data, floatfmt=",.0f"))
@@ -43,8 +27,8 @@ def main():
     duplicate = sum(x["tvl"] for x in data if x["name"] in ["curve.fi/busd", "curve.fi/y"])
     table = [
         *tvl_by_product.items(),
-        ["__duplicate__", duplicate],
-        ["__total__", sum(tvl_by_product.values())],
-        ["__final__", sum(tvl_by_product.values()) - duplicate],
+        [style("duplicate", fg="red"), -duplicate],
+        [style("total", fg="yellow"), sum(tvl_by_product.values())],
+        [style("final", fg="green", bold=True), sum(tvl_by_product.values()) - duplicate],
     ]
     print(tabulate(table, headers=["product", "tvl"], floatfmt=",.0f"))
