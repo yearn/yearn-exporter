09e0e1ba04f0dd7393c5505d3d5affafe3774ac7
nymmrx <nymmrx@protonmail.com>
2021-04-30 21:54:34 +0200

    feat: dockerize historical tvl (#43)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ set -e
 NETWORK="mainnet" # default to Mainnet (Infura)
 EXPLORER="https://api.etherscan.io/api"
 
-if [[ ! -z "${ALCHEMY_URL}" ]]; then
-  if [[ ! $(brownie networks list | grep mainnet-alchemy) ]]; then
-    brownie networks add Ethereum mainnet-alchemy host=$ALCHEMY_URL chainid=1 explorer=$EXPLORER
+if [[ ! -z "${WEB3_PROVIDER}" ]]; then
+  if [[ ! $(brownie networks list | grep mainnet-custom) ]]; then
+    brownie networks add Ethereum mainnet-custom host=$WEB3_PROVIDER chainid=1 explorer=$EXPLORER
   fi
-  NETWORK="mainnet-alchemy"
+  NETWORK="mainnet-custom"
 fi
 
-if [[ -z $1 ]]; then
+if [[ $# -eq 0 ]]; then
   echo "please provide a function to run as first arg."
   exit 1
 fi
 
-echo "Running brownie for $1 on network $NETWORK..."
-brownie run $1 --network $NETWORK
+echo "Running brownie for $@ on network $NETWORK..."
+brownie run $@ --network $NETWORK
