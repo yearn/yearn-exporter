3139f4d2521e22d4ade9eaeb95cb76e6a89faafa
banteg <banteeg@gmail.com>
2021-05-28 09:36:20 +0300

    feat: organize affiliates by project

--- yearn/affiliates.py
+++ yearn/affiliates.py
@@ -44,47 +44,75 @@ class Affiliate:
 
 
 affiliates = [
-    Affiliate(
-        name='inverse-dai-wbtc',
-        vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
-        wrapper='0xB0B02c75Fc1D07d351C991EBf8B5F5b48F24F40B',
-        treasury=None,
+    Partner(
+        name='alchemix',
+        wrappers=[
+            Wrapper(
+                name='alchemix-dai',
+                vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
+                wrapper='0x014dE182c147f8663589d77eAdB109Bf86958f13',
+            ),
+            Wrapper(
+                name='alchemix-dai-2',
+                vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
+                wrapper='0x491EAFC47D019B44e13Ef7cC649bbA51E15C61d7',
+            ),
+        ],
     ),
-    Affiliate(
-        name='inverse-dai-weth',
-        vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
-        wrapper='0x57faa0dec960ed774674a45d61ecfe738eb32052',
-        treasury=None,
+    Partner(
+        name='inverse',
+        wrappers=[
+            Wrapper(
+                name='inverse-dai-wbtc',
+                vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
+                wrapper='0xB0B02c75Fc1D07d351C991EBf8B5F5b48F24F40B',
+            ),
+            Wrapper(
+                name='inverse-dai-weth',
+                vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
+                wrapper='0x57faa0dec960ed774674a45d61ecfe738eb32052',
+            ),
+            Wrapper(
+                name='inverse-usdc-weth',
+                vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
+                wrapper='0x698c1d40574cd90f1920f61D347acCE60D3910af',
+            ),
+        ],
     ),
-    Affiliate(
-        name='inverse-usdc-weth',
-        vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
-        wrapper='0x698c1d40574cd90f1920f61D347acCE60D3910af',
-        treasury=None,
+    Partner(
+        name='frax',
+        wrappers=[
+            Wrapper(
+                name='frax-usdc',
+                vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
+                wrapper='0xEE5825d5185a1D512706f9068E69146A54B6e076',
+            ),
+        ],
     ),
-    Affiliate(
-        name='frax-usdc',
-        vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
-        wrapper='0xEE5825d5185a1D512706f9068E69146A54B6e076',
-        treasury=None,
+    Partner(
+        name='pickle',
+        wrappers=[
+            Wrapper(
+                name='pickle-usdc',
+                vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
+                wrapper='0xEecEE2637c7328300846622c802B2a29e65f3919',
+            ),
+            Wrapper(
+                name='pickle-lusd',
+                vault='0x5fA5B62c8AF877CB37031e0a3B2f34A78e3C56A6',
+                wrapper='0x699cF8fE0C1A6948527cD4737454824c6E3828f1',
+            ),
+        ],
     ),
-    Affiliate(
-        name='alchemix-dai',
-        vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
-        wrapper='0x014dE182c147f8663589d77eAdB109Bf86958f13',
-        treasury=None,
-    ),
-    Affiliate(
-        name='pickle-usdc',
-        vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
-        wrapper='0xEecEE2637c7328300846622c802B2a29e65f3919',
-        treasury=None,
-    ),
-    Affiliate(
-        name='pickle-lusd',
-        vault='0x5fA5B62c8AF877CB37031e0a3B2f34A78e3C56A6',
-        wrapper='0x699cF8fE0C1A6948527cD4737454824c6E3828f1',
-        treasury=None,
+    Partner(
+        name='badger',
+        wrappers=[
+            Wrapper(
+                name='badger-wbtc',
+                vault='0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E',
+                wrapper='0x4b92d19c11435614CD49Af1b589001b7c08cD4D5',
+            ),
+        ],
     ),
 ]
 
