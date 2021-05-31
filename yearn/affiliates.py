294a9670ca599afbe0f6a29857b9e3eea7f233df
banteg <banteeg@gmail.com>
2021-05-31 13:12:05 +0300

    feat: rewrite how affiliates are calculated

--- yearn/affiliates.py
+++ yearn/affiliates.py
@@ -1,20 +1,22 @@
 from bisect import bisect_right
 from dataclasses import dataclass
+from functools import lru_cache
+from pathlib import Path
+from typing import List
+from yearn.utils import get_block_timestamp
 
-from brownie import chain, web3
+import matplotlib.pyplot as plt
+import pandas as pd
+from brownie import web3
 from joblib.parallel import Parallel, delayed
-from toolz import unique
-from tqdm import tqdm
 from web3._utils.abi import filter_by_name
 from web3._utils.events import construct_event_topic_set
 
-from yearn.events import create_filter, decode_logs, get_logs_asap, logs_to_balance_checkpoints
+from yearn.events import decode_logs, get_logs_asap
+from yearn.multicall2 import batch_call
 from yearn.prices import magic
-from yearn.utils import contract_creation_block
 from yearn.v2.vaults import Vault
-import pandas as pd
 
-OPEX = 0.35
 TIERS = {
     0: 0,
     1_000_000: 0.10,
@@ -35,12 +37,105 @@ def get_tier(amount):
     return TIERS[keys[index]]
 
 
+def get_timestamps(blocks):
+    data = Parallel(50, 'threading')(delayed(get_block_timestamp)(block) for block in blocks)
+    return pd.to_datetime([x * 1e9 for x in data])
+
+
+@lru_cache()
+def get_protocol_fees(address):
+    """
+    Get all protocol fee payouts for a given vault.
+
+    Fees can be found as vault share transfers to the rewards address.
+    """
+    vault = Vault.from_address(address)
+    rewards = vault.vault.rewards()
+
+    topics = construct_event_topic_set(
+        filter_by_name('Transfer', vault.vault.abi)[0],
+        web3.codec,
+        {'sender': address, 'receiver': rewards},
+    )
+    logs = decode_logs(get_logs_asap(address, topics))
+    return {log.block_number: log['value'] / vault.scale for log in logs}
+
+
 @dataclass
-class Affiliate:
+class Wrapper:
     name: str
     vault: str
     wrapper: str
+
+    def protocol_fees(self):
+        return get_protocol_fees(self.vault)
+
+    def balances(self, blocks):
+        vault = Vault.from_address(self.vault)
+        balances = batch_call([[vault.vault, 'balanceOf', self.wrapper, block] for block in blocks])
+        return [balance / vault.scale for balance in balances]
+
+    def total_supplies(self, blocks):
+        vault = Vault.from_address(self.vault)
+        supplies = batch_call([[vault.vault, 'totalSupply', block] for block in blocks])
+        return [supply / vault.scale for supply in supplies]
+
+    def vault_prices(self, blocks):
+        prices = Parallel(50, 'threading')(delayed(magic.get_price)(self.vault, block=block) for block in blocks)
+        return prices
+
+
+@dataclass
+class Partner:
+    name: str
+    wrappers: List[Wrapper]
     treasury: str = None
+
+    def process(self):
+        # snapshot wrapper share at each harvest
+        wrappers = []
+        for wrapper in self.wrappers:
+            protocol_fees = wrapper.protocol_fees()
+            blocks, protocol_fees = zip(*protocol_fees.items())
+            wrap = pd.DataFrame(
+                {
+                    'block': blocks,
+                    'timestamp': get_timestamps(blocks),
+                    'protocol_fee': protocol_fees,
+                    'balance': wrapper.balances(blocks),
+                    'total_supply': wrapper.total_supplies(blocks),
+                    'vault_price': wrapper.vault_prices(blocks),
+                }
+            )
+            wrap['balance_usd'] = wrap.balance * wrap.vault_price
+            wrap['share'] = wrap.balance / wrap.total_supply
+            wrap['payout_base'] = wrap.share * wrap.protocol_fee * 0.65
+            wrap['wrapper'] = wrapper.wrapper
+            wrap['vault'] = wrapper.vault
+            wrap = wrap.set_index('block')
+            wrappers.append(wrap)
+            # save a csv for reporting
+            path = Path(f'research/affiliates/{self.name}/{wrapper.name}.csv')
+            path.parent.mkdir(parents=True, exist_ok=True)
+            wrap.to_csv(path)
+
+        # calculate partner fee tier from cummulative wrapper balances
+        partner = pd.concat(wrappers)
+        total_balances = pd.pivot_table(partner, 'balance_usd', 'block', 'vault', 'sum').ffill().sum(axis=1)
+        tiers = total_balances.apply(get_tier).rename('tier')
+        # plot
+        total_balances.plot()
+        tiers.plot(secondary_y=True)
+        path = Path(f'research/affiliates/{self.name}/balance.png')
+        plt.savefig(path, dpi=300)
+
+        # calculate final payout by vault after tier adjustments
+        partner = partner.join(tiers)
+        partner['payout'] = partner.payout_base * partner.tier
+        payouts = partner.groupby('vault').payout.sum()
+        print(payouts)
+        path = Path(f'research/affiliates/{self.name}/payouts.csv')
+        payouts.to_csv(path)
 
 
 affiliates = [
@@ -48,12 +143,12 @@ affiliates = [
         name='alchemix',
         wrappers=[
             Wrapper(
-                name='alchemix-dai',
+                name='dai',
                 vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
                 wrapper='0x014dE182c147f8663589d77eAdB109Bf86958f13',
             ),
             Wrapper(
-                name='alchemix-dai-2',
+                name='dai-2',
                 vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
                 wrapper='0x491EAFC47D019B44e13Ef7cC649bbA51E15C61d7',
             ),
@@ -63,17 +158,17 @@ affiliates = [
         name='inverse',
         wrappers=[
             Wrapper(
-                name='inverse-dai-wbtc',
+                name='dai-wbtc',
                 vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
                 wrapper='0xB0B02c75Fc1D07d351C991EBf8B5F5b48F24F40B',
             ),
             Wrapper(
-                name='inverse-dai-weth',
+                name='dai-weth',
                 vault='0x19D3364A399d251E894aC732651be8B0E4e85001',
                 wrapper='0x57faa0dec960ed774674a45d61ecfe738eb32052',
             ),
             Wrapper(
-                name='inverse-usdc-weth',
+                name='usdc-weth',
                 vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
                 wrapper='0x698c1d40574cd90f1920f61D347acCE60D3910af',
             ),
@@ -83,7 +178,7 @@ affiliates = [
         name='frax',
         wrappers=[
             Wrapper(
-                name='frax-usdc',
+                name='usdc',
                 vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
                 wrapper='0xEE5825d5185a1D512706f9068E69146A54B6e076',
             ),
@@ -93,12 +188,12 @@ affiliates = [
         name='pickle',
         wrappers=[
             Wrapper(
-                name='pickle-usdc',
+                name='usdc',
                 vault='0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9',
                 wrapper='0xEecEE2637c7328300846622c802B2a29e65f3919',
             ),
             Wrapper(
-                name='pickle-lusd',
+                name='lusd',
                 vault='0x5fA5B62c8AF877CB37031e0a3B2f34A78e3C56A6',
                 wrapper='0x699cF8fE0C1A6948527cD4737454824c6E3828f1',
             ),
@@ -108,79 +203,10 @@ affiliates = [
         name='badger',
         wrappers=[
             Wrapper(
-                name='badger-wbtc',
+                name='wbtc',
                 vault='0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E',
                 wrapper='0x4b92d19c11435614CD49Af1b589001b7c08cD4D5',
             ),
         ],
     ),
 ]
-
-
-def get_checkpoints(vault: Vault):
-    from_block = contract_creation_block(str(vault))
-    to_block = chain.height
-    logs = get_logs_asap(str(vault), [vault.topics['Transfer']], from_block, to_block)
-    checkpoints = logs_to_balance_checkpoints(logs)
-    return checkpoints
-
-
-def process_affilliate(affiliate: Affiliate):
-    v = Vault.from_address(affiliate.vault)
-    from_block = contract_creation_block(affiliate.vault)
-    to_block = chain.height
-
-    print('1. fetch rewards addresses')
-    logs = get_logs_asap(affiliate.vault, [v.vault.topics['UpdateRewards']], from_block, to_block)
-    rewards = list(unique(log['rewards'] for log in decode_logs(logs)))
-
-    print('2. fetch all payouts')
-    abi = filter_by_name('Transfer', v.vault.abi)[0]
-    topics = construct_event_topic_set(abi, web3.codec, {'sender': affiliate.vault, 'receiver': rewards})
-    logs = get_logs_asap(affiliate.vault, topics, from_block, to_block)
-    fees = decode_logs(logs)
-    blocks = [int(log.block_number) for log in fees]
-    fees = [log['value'] / v.scale for log in fees]
-
-    print('3. fetch affiliate balances')
-
-    def get_balance(block):
-        return v.vault.balanceOf(affiliate.wrapper, block_identifier=block) / v.scale
-
-    balances = Parallel(10, 'threading')(delayed(get_balance)(block) for block in blocks)
-    print(sum(fees), 'fees')
-    print(len(logs), 'logs')
-    print(max(balances), 'max bal')
-
-    print('4. get total supplies')
-
-    def total_supply(block):
-        return v.vault.totalSupply(block_identifier=block) / v.scale
-
-    supplies = Parallel(10, 'threading')(delayed(total_supply)(block) for block in blocks)
-    print(max(supplies), 'max supply')
-
-    print('5. fetching prices')
-    prices = Parallel(10, 'threading')(delayed(magic.get_price)(affiliate.vault, block=block) for block in blocks)
-
-    print('6. calculating tiers')
-    tiers = [get_tier(balance * price) for balance, price in zip(balances, prices)]
-
-    print('7. calculating payouts')
-    shares = [balance / supply for balance, supply in zip(balances, supplies)]
-    print(max(shares), 'max share')
-    payouts = [fee * share * tier * (1 - OPEX) for fee, share, tier in zip(fees, shares, tiers)]
-    print(sum(payouts), 'to pay')
-
-    df = pd.DataFrame({
-        'block': blocks,
-        'fee': fees,
-        'balance': balances,
-        'supply': supplies,
-        'price': prices,
-        'tier': tiers,
-        'share': shares,
-        'payout': payouts,
-    }).set_index('block')
-    df.to_csv(f'research/affiliates/{affiliate.name}.csv')
-    return df
