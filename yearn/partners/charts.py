5794b4e304969e89abdf64112c171b5701571c8f
banteg <banteeg@gmail.com>
2021-06-04 12:48:40 +0300

    feat(chart): add concise currency fmt, tier ticks

--- yearn/partners/charts.py
+++ yearn/partners/charts.py
@@ -1,10 +1,30 @@
+from bisect import bisect_right
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.ticker import FuncFormatter, PercentFormatter
+from matplotlib.ticker import PercentFormatter
+from yearn.partners.constants import TIERS
 
 colors = {'blue': '#0657F9', 'yellow': '#FABF06'}
+
+
+def currency_formatter(value, index):
+    """
+    Concise currency formatter: $500, $10k, $500k, $1m, $2.5m
+    """
+    powers = [10 ** x for x in (3, 6, 9, 12)]
+    suffixes = ['k', 'm', 'b', 't']
+
+    if value < powers[0]:
+        return f'${value:.0f}'
+
+    i = bisect_right(powers, value) - 1
+
+    if float(f'{value / powers[i - 1]:.1f}') == 1000 or value < powers[1]:
+        return f'${value / powers[i]:.0f}{suffixes[i]}'
+    else:
+        return f'${value / powers[i]:.1f}{suffixes[i]}'
 
 
 def make_partner_charts(partner, data):
@@ -16,25 +36,35 @@ def make_partner_charts(partner, data):
     ax = list(ax)
 
     # aggregate balance of wrappers
-    agg_balance = pd.pivot_table(df, 'balance_usd', 'timestamp', 'vault', 'sum').ffill().sum(axis=1).resample('1D').mean().ffill()
+    agg_balance = (
+        pd.pivot_table(df, 'balance_usd', 'timestamp', 'vault', 'sum').ffill().sum(axis=1).resample('1D').mean().ffill()
+    )
     agg_balance.plot(title=f'yearn x {partner.name}', label='balance, usd', legend=True, ax=ax[0], c=colors['blue'])
 
     # tier assigned at the end of each day
     daily_tier = df.resample('1D').last().tier.ffill()
-    ax.append(daily_tier.plot(label='partner tier', ax=ax[0], secondary_y=True, legend=True, c=colors['yellow'], drawstyle="steps-post"))
+    ax.append(
+        daily_tier.plot(
+            label='partner tier', ax=ax[0], secondary_y=True, legend=True, c=colors['yellow'], drawstyle="steps-post"
+        )
+    )
 
     # accumulated earnings
     earnings = (df.vault_price * df.payout).resample('1D').sum().cumsum().ffill()
     earnings.plot(label='earnings, usd', legend=True, ax=ax[1], c=colors['blue'])
-    
+
     # share of gross revenue
     ratio = (df.payout / df.protocol_fee).resample('1D').mean().ffill()
     ax.append(ratio.plot(label='share of revenue', legend=True, ax=ax[1], secondary_y=True, c=colors['yellow']))
-    
+
     # trim start date to the first earnings
     xmin = earnings[earnings > 0].index[0] - pd.to_timedelta('2D')
     ax[0].set_xlim(xmin=xmin)
     ax[1].set_xlim(xmin=xmin)
+
+    # only show ticks for valid affiliate tiers
+    ax[2].set_yticks(sorted(TIERS.values()))
+    ax[2].autoscale_view()
 
     # axis labels
     ax[0].set_ylabel('aggregate wrapper balance')
@@ -43,8 +73,8 @@ def make_partner_charts(partner, data):
     ax[3].set_ylabel('share of gross revenue')
 
     # axis formatters
-    ax[0].yaxis.set_major_formatter(FuncFormatter(lambda value, index: f'${value / 1e6:,.0f}m'))
-    ax[1].yaxis.set_major_formatter(FuncFormatter(lambda value, index: f'${value / 1e3:,.0f}k'))
+    ax[0].yaxis.set_major_formatter(currency_formatter)
+    ax[1].yaxis.set_major_formatter(currency_formatter)
     ax[2].yaxis.set_major_formatter(PercentFormatter(1))
     ax[3].yaxis.set_major_formatter(PercentFormatter(1))
 
