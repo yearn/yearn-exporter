e5ec37494f33352420843000a6bee38b6ec6fb55
dudesahn <dudesahn@gmail.com>
2021-06-09 17:27:08 -0400

    fix: update to use correct variables for fees

--- yearn/apy/v2.py
+++ yearn/apy/v2.py
@@ -77,7 +77,7 @@ def simple(vault: Vault, samples: ApySamples) -> Apy:
     apr_after_fees = compounding * ((net_apy + 1) ** (1 / compounding)) - compounding
     
     # calculate our pre-fee APR
-    pre_fee_apr = apr_after_fees / (1 - perf_fee) + mgmt_fee
+    pre_fee_apr = apr_after_fees / (1 - performance) + management
 
     points = ApyPoints(week_ago_apy, month_ago_apy, inception_apy)
     fees = ApyFees(performance=performance, management=management)
@@ -131,7 +131,7 @@ def average(vault: Vault, samples: ApySamples) -> Apy:
     apr_after_fees = compounding * ((net_apy + 1) ** (1 / compounding)) - compounding
     
     # calculate our pre-fee APR
-    pre_fee_apr = apr_after_fees / (1 - perf_fee) + mgmt_fee
+    pre_fee_apr = apr_after_fees / (1 - performance) + management
     
     points = ApyPoints(week_ago_apy, month_ago_apy, inception_apy)
     fees = ApyFees(performance=performance, management=management)
