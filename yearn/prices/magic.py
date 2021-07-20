410882705e02af95efd1852cc963e3801c8c6b28
Marius <marius.pod@gmail.com>
2021-07-20 12:37:27 +0200

    fix(magic): set default price in case no price could be discovered in any market.

--- yearn/prices/magic.py
+++ yearn/prices/magic.py
@@ -66,9 +66,7 @@ def get_price(token, block=None):
         price = uniswap.get_price_v1(token, block=block)
         logger.debug("uniswap v1 -> %s", price)
     if not price:
-        logger.error("failed to get price for %s", token)
-
-    if not price:
-        raise PriceError(f'could not fetch price for {token}')
+        price = 0
+        logger.error("failed to get price for %s, setting price=0", token)
 
     return price
