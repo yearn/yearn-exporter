9600b9dd524e81bcf8215b915c33132a496e5ef2
banteg <banteeg@gmail.com>
2021-09-02 02:24:45 +0300

    fix: stricter test for yearn like vaults

--- yearn/prices/yearn.py
+++ yearn/prices/yearn.py
@@ -1,28 +1,31 @@
-from brownie import Contract
-from yearn.multicall2 import fetch_multicall
+from brownie import multicall
+
+from yearn.utils import contract
 
 
 def is_yearn_vault(token):
-    vault = Contract(token)
-    return hasattr(vault, 'pricePerShare') or hasattr(vault, 'getPricePerFullShare')
+    vault = contract(token)
+    return any(
+        all(hasattr(vault, attr) for attr in kind)
+        for kind in [
+            ['pricePerShare', 'token', 'decimals'],
+            ['getPricePerFullShare', 'token'],
+        ]
+    )
 
 
 def get_price(token, block=None):
     # v1 vaults use getPricePerFullShare scaled to 18 decimals
     # v2 vaults use pricePerShare scaled to underlying token decimals
-    vault = Contract(token)
+    vault = contract(token)
     if hasattr(vault, 'pricePerShare'):
-        share_price, underlying, decimals = fetch_multicall(
-            [vault, 'pricePerShare'],
-            [vault, 'token'],
-            [vault, 'decimals'],
-            block=block
-        )
+        with multicall:
+            share_price = vault.pricePerShare()
+            underlying = vault.token()
+            decimals = vault.decimals()
         return [share_price / 10 ** decimals, underlying]
     if hasattr(vault, 'getPricePerFullShare'):
-        share_price, underlying = fetch_multicall(
-            [vault, 'getPricePerFullShare'],
-            [vault, 'token'],
-            block=block
-        )
+        with multicall:
+            share_price = vault.getPricePerFullShare()
+            underlying = vault.token()
         return [share_price / 1e18, underlying]
