e84f4a781928c9dc65048a14b357f27f2d1404d9
Marius <marius.pod@gmail.com>
2021-09-28 21:17:34 +0200

    feat: adding a historical exporter that goes backwards and first fills the daily intervals and then the hourly intervals. Refactored to use always the same code path to export data to victoria.

--- scripts/exporter.py
+++ scripts/exporter.py
@@ -1,13 +1,12 @@
 import logging
 import os
 import time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 
 from brownie import chain
 from yearn.outputs import victoria
 from yearn.yearn import Yearn
 from yearn.utils import closest_block_after_timestamp
-
 from itertools import count
 
 
@@ -17,12 +16,16 @@ sleep_interval = int(os.environ.get('SLEEP_SECONDS', '0'))
 def main():
     yearn = Yearn()
     for block in chain.new_blocks(height_buffer=1):
-        start = time.time()
-        data = yearn.describe(block.number)
-        victoria.export(block.timestamp, data)
-        tvl = sum(vault['tvl'] for product in data.values() for vault in product.values())
-        logger.info('exported block=%d tvl=%.0f took=%.3fs', block.number, tvl, time.time() - start)
+        export(yearn, block.number, block.timestamp)
         time.sleep(sleep_interval)
+
+
+def export(yearn, block_number, block_ts):
+    start = time.time()
+    data = yearn.describe(block_number)
+    victoria.export(block_ts, data)
+    tvl = sum(vault['tvl'] for product in data.values() for vault in product.values())
+    logger.info('exported block=%d tvl=%.0f took=%.3fs', block_number, tvl, time.time() - start)
 
 
 def tvl():
@@ -35,25 +38,35 @@ def tvl():
         time.sleep(sleep_interval)
 
 
-# start: 2020-02-12 first iearn deployment
-def historical(start=datetime(2020, 2, 12, tzinfo=timezone.utc), end=datetime.now(tz=timezone.utc)):
-    yearn = Yearn(load_strategies=False)
-    interval = timedelta(hours=1)
+# end: 2020-02-12 first iearn deployment
+def historical_export(start=datetime.now(), end=datetime(2020, 2, 12)):
+    yearn = Yearn()
 
-    for snapshot in _generate_snapshot_range(start, interval):
-        if snapshot > end:
-            logger.info("done restoring historical data")
-            break
-        start = time.time()
-        block = closest_block_after_timestamp(snapshot.timestamp())
-        assert block is not None, "no block after timestamp found"
-        data = yearn.describe(block)
-        victoria.export(snapshot.timestamp(), data)
-        tvl = sum(vault['tvl'] for product in data.values() for vault in product.values())
+    start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+    daily = _generate_snapshot_range(start, end, timedelta(days=1))
+    for day in daily:
+        _interval_export(yearn, day)
 
-        logger.info("inserted snapshot=%s tvl=%.0f took=%.3fs", snapshot, tvl, time.time() - start)
+    hourly = _generate_snapshot_range(start, end, timedelta(hours=1))
+    for hour in hourly:
+        _interval_export(yearn, hour, skip_midnight=True)
 
 
-def _generate_snapshot_range(start, interval):
+def _interval_export(yearn, snapshot, skip_midnight=False):
+    if skip_midnight and snapshot.hour == 0 and snapshot.minute == 0:
+        return
+
+    ts = snapshot.timestamp()
+    block = closest_block_after_timestamp(ts)
+    assert block is not None, "no block after timestamp found"
+    export(yearn, block, ts)
+    logger.info("exported historical snapshot %s", snapshot)
+
+
+def _generate_snapshot_range(start, end, interval):
     for i in count():
-        yield start + i * interval
+        snapshot = start - i * interval
+        if snapshot < end:
+            return
+        else:
+            yield snapshot
