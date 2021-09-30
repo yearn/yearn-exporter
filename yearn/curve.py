eaf0de8588ddd99e8b3bef0bff7616ab2495f60b
banteg <banteeg@gmail.com>
2021-09-30 14:11:08 +0300

    Revert "fix: better handling for getting the curve pool during #calculate_apy."
    This reverts commit 7ed48e334e5b2837f75f761adf7e53aa8be232fa.

--- yearn/curve.py
+++ yearn/curve.py
@@ -54,28 +54,16 @@ def calculate_boost(gauge, addr, block=None):
 
 def calculate_apy(gauge, lp_token, block=None):
     crv_price = magic.get_price(crv)
-    pool_address = curve.get_pool(lp_token)
-    # TODO check if that's correct
-    calls = [
+    pool = Contract(curve.get_pool(lp_token))
+    results = fetch_multicall(
         [gauge, "working_supply"],
         [gauge_controller, "gauge_relative_weight", gauge],
         [gauge, "inflation_rate"],
-    ]
-    if pool_address:
-        pool = Contract(pool_address)
-        calls.append([pool, "get_virtual_price"])
-
-    results = fetch_multicall(
-        *calls,
+        [pool, "get_virtual_price"],
         block=block,
     )
     results = [x / 1e18 for x in results]
-    if pool_address:
-        working_supply, relative_weight, inflation_rate, virtual_price = results
-    else:
-        working_supply, relative_weight, inflation_rate = results
-        virtual_price = 0
-
+    working_supply, relative_weight, inflation_rate, virtual_price = results
     token_price = magic.get_price(lp_token, block=block)
     try:
         rate = (inflation_rate * relative_weight * 86400 * 365 / working_supply * 0.4) / token_price
