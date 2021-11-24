e9f0be304e77d7ff5d25e72bb2dc51c667efd54f
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2021-11-24 23:46:34 +0000

    fix: historical treasury exporter start date

--- scripts/historical_treasury_exporter.py
+++ scripts/historical_treasury_exporter.py
@@ -25,8 +25,8 @@ CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 50))
 
 def main():
     start = datetime.now(tz=timezone.utc)
-    # end: 2020-02-12 first iearn deployment
-    end = datetime(2020, 2, 12, tzinfo=timezone.utc)
+    # end: 2020-02-12 first treasury tx
+    end = datetime(2020, 7, 21, tzinfo=timezone.utc)
 
     interval_map = [
         {
