0f8feacb2b14c92d653ead43b56fec5488a9dd9a
banteg <banteeg@gmail.com>
2021-11-26 22:11:36 +0300

    fix: update compound for multichain

--- yearn/prices/compound.py
+++ yearn/prices/compound.py
@@ -1,36 +1,53 @@
-from yearn.utils import contract
-from cachetools.func import ttl_cache
+from brownie import chain
+from yearn.exceptions import UnsupportedNetwork
+from yearn.utils import Singleton, contract
 from yearn.multicall2 import fetch_multicall
+from yearn.networks import Network
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-@ttl_cache(ttl=3600)
-def get_markets():
-    comptroller = contract("0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B")
-    creamtroller = contract('0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258')
-    ironbankroller = contract("0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB")
-
-    results = fetch_multicall(
-        [comptroller, 'getAllMarkets'],
-        [creamtroller, 'getAllMarkets'],
-        [ironbankroller, 'getAllMarkets'],
-    )
-    names = ['compound', 'cream', 'ironbank']
-    return dict(zip(names, results))
+addresses = {
+    Network.Mainnet: {
+        'compound': '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
+        'cream': '0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258',
+        'ironbank': '0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB',
+    }
+}
 
 
-def is_compound_market(token):
-    markets = get_markets()
-    return any(token in market for market in markets.values())
+class Compound(metaclass=Singleton):
+    def __init__(self):
+        if chain.id not in addresses:
+            raise UnsupportedNetwork('compound is not supported on this network')
+
+        results = fetch_multicall(*[
+            [contract(addr), 'getAllMarkets']
+            for addr in addresses[chain.id].values()
+        ])
+        self.markets = dict(zip(addresses[chain.id], results))
+        log_counts = ', '.join(f'{len(self.markets[name])} {name}' for name in self.markets)
+        logger.info(f'loaded {log_counts} markets')
+
+    def is_compound_market(self, token):
+        return any(token in market for market in self.markets.values())
+
+    def get_price(self, token, block=None):
+        token = contract(token)
+        underlying, exchange_rate, decimals = fetch_multicall(
+            [token, 'underlying'],
+            [token, 'exchangeRateCurrent'],
+            [token, 'decimals'],
+            block=block,
+        )
+        exchange_rate /= 1e18
+        under_decimals = contract(underlying).decimals()
+        return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
 
 
-def get_price(token, block=None):
-    token = contract(token)
-    underlying, exchange_rate, decimals = fetch_multicall(
-        [token, 'underlying'],
-        [token, 'exchangeRateCurrent'],
-        [token, 'decimals'],
-        block=block,
-    )
-    exchange_rate /= 1e18
-    under_decimals = contract(underlying).decimals()
-    return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
+compound = None
+try:
+    compound = Compound()
+except UnsupportedNetwork:
+    pass
