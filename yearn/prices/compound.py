9d66968dcd3cd899a2dba13777364162a3b66770
banteg <banteeg@gmail.com>
2021-11-27 22:22:05 +0300

    feat: rewrite compound like markets with native oracle and multiplexer

--- yearn/prices/compound.py
+++ yearn/prices/compound.py
@@ -1,9 +1,16 @@
+from functools import cached_property
+from sys import base_prefix
+from typing import Optional
 from brownie import chain, Contract
 from yearn.exceptions import UnsupportedNetwork
 from yearn.utils import Singleton, contract
 from yearn.multicall2 import fetch_multicall
 from yearn.networks import Network
 import logging
+from cachetools.func import ttl_cache
+from dataclasses import dataclass
+from yearn.prices.constants import usdc, weth
+from yearn.prices import magic
 
 logger = logging.getLogger(__name__)
 
@@ -15,56 +22,153 @@ def get_fantom_ironbank():
     return Contract.from_abi(unitroller._name, str(unitroller), abi=implementation.abi)
 
 
+@dataclass
+class CompoundConfig:
+    name: str
+    address: str
+    oracle_base: str = usdc
+
+
 addresses = {
-    Network.Mainnet: {
-        'compound': '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
-        'cream': '0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258',
-        'ironbank': '0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB',
-    },
-    Network.Fantom: {
-        'ironbank': get_fantom_ironbank,
-    },
-    Network.Arbitrum: {
-        'ironbank': '0xbadaC56c9aca307079e8B8FC699987AAc89813ee',
-    },
+    Network.Mainnet: [
+        CompoundConfig(
+            name='compound',
+            address='0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
+        ),
+        CompoundConfig(
+            name='cream',
+            address='0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258',
+            oracle_base=weth,
+        ),
+        CompoundConfig(
+            name='ironbank',
+            address='0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB',
+        ),
+    ],
+    Network.Fantom: [
+        CompoundConfig(
+            name='ironbank',
+            address=get_fantom_ironbank,
+        ),
+    ],
+    Network.Arbitrum: [
+        CompoundConfig(
+            name='ironbank',
+            address='0xbadaC56c9aca307079e8B8FC699987AAc89813ee',
+        ),
+    ],
 }
 
 
-class Compound(metaclass=Singleton):
-    def __init__(self):
-        if chain.id not in addresses:
-            raise UnsupportedNetwork('compound is not supported on this network')
-
-        results = fetch_multicall(
-            *[
-                [contract(addr) if isinstance(addr, str) else addr(), 'getAllMarkets']
-                for addr in addresses[chain.id].values()
-            ]
-        )
-        self.markets = dict(zip(addresses[chain.id], results))
-        log_counts = ', '.join(
-            f'{len(self.markets[name])} {name}' for name in self.markets
-        )
-        logger.info(f'loaded {log_counts} markets')
-
-    def __contains__(self, token):
-        return any(token in market for market in self.markets.values())
+@dataclass
+class CompoundOracle:
+    oracle: str
+    base: str
 
     def get_price(self, token, block=None):
-        token = contract(token)
-        underlying, exchange_rate, decimals = fetch_multicall(
-            [token, 'underlying'],
-            [token, 'exchangeRateCurrent'],
-            [token, 'decimals'],
-            block=block,
+        price = self.oracle.getUnderlyingPrice(
+            token, block_identifier=block
+        ) / 10 ** (36 - self.under_decimals)
+        if self.base != usdc:
+            price *= magic.get_price(self.base, block=block)
+        return price
+
+
+@dataclass
+class CompoundMarket:
+    token: str
+    oracle: str
+
+    @cached_property
+    def name(self):
+        return self.ctoken.symbol()
+
+    @cached_property
+    def ctoken(self):
+        return contract(self.token)
+
+    @cached_property
+    def underlying(self):
+        match self.token:
+            case '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5' | '0xD06527D5e56A3495252A528C4987003b712860eE':
+                return contract('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')  # ceth | creth -> weth
+            case _: return contract(self.ctoken.underlying())
+
+    @cached_property
+    def cdecimals(self):
+        return self.ctoken.decimals()
+
+    @cached_property
+    def under_decimals(self):
+        return self.underlying.decimals()
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.token == other
+        elif isinstance(other, CompoundMarket):
+            return self.token == other.token
+
+        raise TypeError('can only compare to [str, CompoundMarket]')
+
+    def get_exchange_rate(self, block=None):
+        exchange_rate = (
+            self.ctoken.exchangeRateCurrent.call(block_identifier=block) / 1e18
         )
-        exchange_rate /= 1e18
-        under_decimals = contract(underlying).decimals()
-        return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
+        return exchange_rate * 10 ** (self.cdecimals - self.under_decimals)
+
+    def get_underlying_price(self, block=None):
+        price = self.oracle.getUnderlyingPrice(
+            self.token, block_identifier=block
+        ) / 10 ** (36 - self.under_decimals)
+        return price
+
+
+class Compound:
+    def __init__(self, name, unitroller, oracle_base):
+        self.name = name
+        self.unitroller = contract(unitroller)
+        self.oracle = contract(self.unitroller.oracle())
+        self.oracle_base = oracle_base
+        self.markets  # load markets on init
+
+    def __repr__(self):
+        return f'<Compound name={self.name} unitroller={self.unitroller} oracle={self.oracle}>'
+
+    @property
+    @ttl_cache(ttl=3600)
+    def markets(self):
+        all_markets = self.unitroller.getAllMarkets()
+        markets = [CompoundMarket(token, self.oracle) for token in all_markets]
+        logger.info(f'loaded {len(markets)} {self.name} markets')
+        return markets
+
+    def get_price(self, token, block=None):
+        market = next(x for x in self.markets if x == token)
+        exchange_rate = market.get_exchange_rate(block)
+        underlying_price = market.get_underlying_price(block)
+        base_price = magic.get_price(self.oracle_base, block)
+        return underlying_price * exchange_rate * base_price
+
+
+class CompoundMultiplexer(metaclass=Singleton):
+    def __init__(self):
+        if chain.id not in addresses:
+            raise UnsupportedNetwork('uniswap v2 is not supported on this network')
+        self.compounds = [
+            Compound(conf.name, conf.address, conf.oracle_base)
+            for conf in addresses[chain.id]
+        ]
+
+    def __contains__(self, token):
+        return any(token in comp.markets for comp in self.compounds)
+
+    def get_price(self, token, block=None):
+        comp = next(comp for comp in self.compounds if token in comp.markets)
+        return comp.get_price(token, block)
 
 
 compound = None
 try:
-    compound = Compound()
+    compound = CompoundMultiplexer()
 except UnsupportedNetwork:
     pass
