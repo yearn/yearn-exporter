b45e6c9517fa49f66f4ca9eea9ea9c0fe7aaf819
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2021-12-17 03:30:46 +0000

    feat: merge tx analysis with existing exporter

--- yearn/outputs/victoria.py
+++ yearn/outputs/victoria.py
@@ -13,12 +13,12 @@ mapping = {
     "earn": {
         "metric": "iearn",
         "labels": ["vault", "param", "address", "version"],
-        "agg_stats": ["total wallets"]
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
     },
     "ib": {
         "metric": "ironbank",
         "labels": ["vault", "param", "address", "version"],
-        "agg_stats": ["total wallets"]
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
     },
     "v1": {
         "metric": "yearn",
@@ -37,12 +37,23 @@ mapping = {
     "special": {
         "metric": "yearn_vault",
         "labels": ["vault", "param", "address", "version", "experimental"],
-        "agg_stats": ["total wallets"]
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
     }
 }
 
 
 def export(timestamp, data):
+    metrics_to_export = []
+    for key, value in data['agg_stats'].items():
+        if key != 'user balances usd':
+            label_names = ['param']
+            label_values = [key]
+            item = _build_item("aggregate", label_names, label_values, value, timestamp)
+            print(item)
+            metrics_to_export.append(item)
+
+    # for testing
+    _post(metrics_to_export)
     metrics_to_export = []
 
     if Network(chain.id) == Network.Fantom:
@@ -52,32 +63,68 @@ def export(timestamp, data):
 
     for product in simple_products:
         metric = mapping[product]["metric"]
-        for vault, params in data[product].items():
+        for k, v in data[product].items():
+            if k in mapping[product]["agg_stats"]:
+                label_names = ['param']
+                label_values = [k]
+                item = _build_item(metric, label_names, label_values, v, timestamp)
+                print(item)
+                metrics_to_export.append(item)
+            else:
+                vault, params = k, v
+                for key, value in params.items():
+                    if key in ["address", "version", "experimental"] or value is None:
+                        continue
+                    
+                    has_experiments = product == "special"
 
+                    if key == "user balances":
+                        for user, bals in value.items():
+                            for denom, bal in bals.items():
+                                label_values = [user] + _get_label_values(params, [vault, denom], has_experiments)
+                                label_names = ["user"] + mapping[product]["labels"]
+                                item = _build_item(metric, label_names, label_values, bal, timestamp)
+                                print(item)
+                                metrics_to_export.append(item)
+                    else:
+                        label_values = _get_label_values(params, [vault, key], has_experiments)
+                        label_names = mapping[product]["labels"]
+
+                        item = _build_item(metric, label_names, label_values, value, timestamp)
+                        metrics_to_export.append(item)
+
+    # for testing
+    _post(metrics_to_export)
+    metrics_to_export = []
+
+    for k, v in data["v2"].items():
+        metric = mapping["v2"]["metric"]
+        if k in mapping["v2"]["agg_stats"]:
+            label_names = ['param']
+            label_values = [k]
+            item = _build_item(metric, label_names, label_values, v, timestamp)
+            print(item)
+            metrics_to_export.append(item)
+        else: 
+            vault, params = k, v
             for key, value in params.items():
-                if key in ["address", "version", "experimental"] or value is None:
+                if key in ["address", "version", "experimental", "strategies"] or value is None or type(value) == dict:
                     continue
 
-                has_experiments = product == "special"
+                if key == "user balances":
+                    for user, bals in value.items():
+                        for denom, bal in bals.items():
+                            label_values = [user] + _get_label_values(params, [vault, denom], has_experiments)
+                            label_names = ["user"] + mapping[product]["labels"]
+                            item = _build_item(metric, label_names, label_values, bal, timestamp)
+                            print(item)
+                            metrics_to_export.append(item)
+                else:
+                    label_values = _get_label_values(params, [vault, key], True)
+                    label_names = mapping["v2"]["labels"]
 
-                label_values = _get_label_values(params, [vault, key], has_experiments)
-                label_names = mapping[product]["labels"]
-
-                item = _build_item(metric, label_names, label_values, value, timestamp)
-                metrics_to_export.append(item)
-
-
-    for vault, params in data["v2"].items():
-        metric = mapping["v2"]["metric"]
-        for key, value in params.items():
-            if key in ["address", "version", "experimental", "strategies"] or value is None:
-                continue
-
-            label_values = _get_label_values(params, [vault, key], True)
-            label_names = mapping["v2"]["labels"]
-
-            item = _build_item(metric, label_names, label_values, value, timestamp)
-            metrics_to_export.append(item)
+                    item = _build_item(metric, label_names, label_values, value, timestamp)
+                    metrics_to_export.append(item)
 
         # strategies can have nested structs
         metric = mapping["v2_strategy"]["metric"]
--- yearn/yearn.py
+++ yearn/yearn.py
@@ -3,6 +3,7 @@ from collections import Counter
 from time import time
 
 from brownie import chain
+from collections import Counter
 from joblib import Parallel, delayed
 
 import yearn.iearn
@@ -23,24 +24,24 @@ class Yearn:
     Can describe all products.
     """
 
-    def __init__(self, load_strategies=True, load_harvests=False, watch_events_forever=True) -> None:
+    def __init__(self, load_strategies=True, load_harvests=False, load_transfers=False, watch_events_forever=True) -> None:
         start = time()
         if chain.id == Network.Mainnet:
             self.registries = {
                 "earn": yearn.iearn.Registry(),
-                "v1": yearn.v1.registry.Registry(),
+                "v1": yearn.v1.registry.Registry(watch_events_forever=watch_events_forever),
                 "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(),
                 "special": yearn.special.Registry(),
             }
         elif chain.id ==  Network.Fantom:
             self.registries = {
-                "v2": yearn.v2.registry.Registry(),
+                "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(),
             }
         elif chain.id == Network.Arbitrum:
             self.registries = {
-                "v2": yearn.v2.registry.Registry(),
+                "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(),
             }
         else:
@@ -50,6 +51,9 @@ class Yearn:
             self.registries["v2"].load_strategies()
         if load_harvests:
             self.registries["v2"].load_harvests()
+        if load_transfers:
+            self.registries['v1'].load_transfers()
+            self.registries['v2'].load_transfers()
         logger.info('loaded yearn in %.3fs', time() - start)
 
     def describe(self, block=None):
@@ -57,7 +61,24 @@ class Yearn:
             delayed(self.registries[key].describe)(block=block)
             for key in self.registries
         )
-        return dict(zip(self.registries, desc))
+        results_dict = dict(zip(self.registries, desc))
+        user_balances = Counter()
+        for registry in desc:
+            for vault, data in registry.items():
+                try:
+                    for user, bals in data['user balances'].items():
+                        user_balances[user] += bals["usd balance"]
+                except: # process vaults, not aggregated stats
+                    pass # TODO: add total users and user balances for earn, ib, special
+        print(len(user_balances))
+        agg_stats = {
+            "agg_stats": {
+                "total users": len(user_balances),
+                "user balances usd": user_balances
+            }
+        }
+        results_dict.update(agg_stats)
+        return results_dict
 
     def describe_wallets(self, block=None):
         registries = ['v1','v2'] # TODO: add other registries [earn, ib, special]
