55a2362698e5258ee10b7cd21295300face37914
Marius <14898268+mariuspod@users.noreply.github.com>
2022-01-06 22:29:17 +0100

    feat: integrate sentry.

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,5 +1,7 @@
 from yearn.logs import setup_logging
 from yearn.middleware.middleware import setup_middleware
+from yearn.sentry import setup_sentry
 
 setup_logging()
 setup_middleware()
+setup_sentry()
