7609c63e962e4f0c4c8fd6088cbc4d065bba8784
Marius <14898268+mariuspod@users.noreply.github.com>
2022-01-06 23:37:15 +0100

    feat: make sentry optional.

--- yearn/sentry.py
+++ yearn/sentry.py
@@ -3,24 +3,22 @@ from sentry_sdk.integrations.threading import ThreadingIntegration
 import os
 
 environment = os.getenv('ENV', 'DEVELOPMENT')
-sentry_dsn = os.getenv('SENTRY_DSN')
-if not sentry_dsn:
-    print("Please set SENTRY_DSN env variable!")
-    exit(1)
 
 def before_send(event, hint):
     # custom event parsing goes here
     return event
 
 def setup_sentry():
-    init(
-        sentry_dsn,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-        environment=environment,
-        before_send=before_send,
-        debug=False,
-        integrations=[ThreadingIntegration(propagate_hub=True)]
-    )
+    sentry_dsn = os.getenv('SENTRY_DSN')
+    if sentry_dsn:
+        init(
+            sentry_dsn,
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            # We recommend adjusting this value in production.
+            traces_sample_rate=1.0,
+            environment=environment,
+            before_send=before_send,
+            debug=False,
+            integrations=[ThreadingIntegration(propagate_hub=True)]
+        )
