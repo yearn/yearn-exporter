f91520b2f82ea600f43f25541a697ec73c079600
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2022-01-16 17:10:49 +0000

    fix: vault wallet exporter

--- yearn/outputs/describers/vault.py
+++ yearn/outputs/describers/vault.py
@@ -8,6 +8,8 @@ from yearn.exceptions import PriceError
 
 logger = logging.getLogger(__name__)
 
+from yearn.outputs.postgres.utils import fetch_balances
+
 class VaultWalletDescriber:
     def wallets(self, vault_address, block=None):
         return self.wallet_balances(vault_address, block=block).keys()
