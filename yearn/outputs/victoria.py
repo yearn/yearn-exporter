e8b25a1897e2c15cd2a0c3ecfffbd127993c3fa2
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2022-01-16 17:10:49 +0000

    feat: merge tx analysis with existing exporter

---
+++ yearn/outputs/victoria.py
@@ -0,0 +1,297 @@
+import requests
+import os
+import gzip
+import math
+import json
+from typing import List, Dict
+from brownie import Contract, chain
+from yearn.treasury.buckets import get_token_bucket
+from yearn.utils import contract
+from yearn.networks import Network
+
+mapping = {
+    "earn": {
+        "metric": "iearn",
+        "labels": ["vault", "param", "address", "version"],
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
+    },
+    "ib": {
+        "metric": "ironbank",
+        "labels": ["vault", "param", "address", "version"],
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
+    },
+    "v1": {
+        "metric": "yearn",
+        "labels": ["vault", "param", "address", "version"],
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
+    },
+    "v2": {
+        "metric": "yearn_vault",
+        "labels": ["vault", "param", "address", "version", "experimental"],
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
+    },
+    "v2_strategy": {
+        "metric": "yearn_strategy",
+        "labels": ["vault", "strategy", "param", "address", "version", "experimental"],
+    },
+    "special": {
+        "metric": "yearn_vault",
+        "labels": ["vault", "param", "address", "version", "experimental"],
+        "agg_stats": ["total wallets","active wallets","wallets > $5k","wallets > $50k"]
+    }
+}
+
+
+def export(timestamp, data):
+    metrics_to_export = []
+    for key, value in data['agg_stats'].items():
+        if key != 'user balances usd':
+            label_names = ['param']
+            label_values = [key]
+            item = _build_item("aggregate", label_names, label_values, value, timestamp)
+            print(item)
+            metrics_to_export.append(item)
+
+    # for testing
+    _post(metrics_to_export)
+    metrics_to_export = []
+
+    if Network(chain.id) == Network.Fantom:
+        simple_products = ["ib"]
+    else:
+        simple_products = ["v1", "earn", "ib", "special"]
+
+    for product in simple_products:
+        metric = mapping[product]["metric"]
+        for k, v in data[product].items():
+            if k in mapping[product]["agg_stats"]:
+                label_names = ['param']
+                label_values = [k]
+                item = _build_item(metric, label_names, label_values, v, timestamp)
+                print(item)
+                metrics_to_export.append(item)
+            else:
+                vault, params = k, v
+                for key, value in params.items():
+                    if key in ["address", "version", "experimental"] or value is None:
+                        continue
+                    
+                    has_experiments = product == "special"
+
+                    if key == "user balances":
+                        for user, bals in value.items():
+                            for denom, bal in bals.items():
+                                label_values = [user] + _get_label_values(params, [vault, denom], has_experiments)
+                                label_names = ["user"] + mapping[product]["labels"]
+                                item = _build_item(metric, label_names, label_values, bal, timestamp)
+                                print(item)
+                                metrics_to_export.append(item)
+                    else:
+                        label_values = _get_label_values(params, [vault, key], has_experiments)
+                        label_names = mapping[product]["labels"]
+
+                        item = _build_item(metric, label_names, label_values, value, timestamp)
+                        metrics_to_export.append(item)
+
+    # for testing
+    _post(metrics_to_export)
+    metrics_to_export = []
+
+    for k, v in data["v2"].items():
+        metric = mapping["v2"]["metric"]
+        if k in mapping["v2"]["agg_stats"]:
+            label_names = ['param']
+            label_values = [k]
+            item = _build_item(metric, label_names, label_values, v, timestamp)
+            print(item)
+            metrics_to_export.append(item)
+        else: 
+            vault, params = k, v
+            for key, value in params.items():
+                if key in ["address", "version", "experimental", "strategies"] or value is None or type(value) == dict:
+                    continue
+
+                if key == "user balances":
+                    for user, bals in value.items():
+                        for denom, bal in bals.items():
+                            label_values = [user] + _get_label_values(params, [vault, denom], has_experiments)
+                            label_names = ["user"] + mapping[product]["labels"]
+                            item = _build_item(metric, label_names, label_values, bal, timestamp)
+                            print(item)
+                            metrics_to_export.append(item)
+                else:
+                    label_values = _get_label_values(params, [vault, key], True)
+                    label_names = mapping["v2"]["labels"]
+
+                    item = _build_item(metric, label_names, label_values, value, timestamp)
+                    metrics_to_export.append(item)
+
+        # strategies can have nested structs
+        metric = mapping["v2_strategy"]["metric"]
+        for strategy, strategy_params in data["v2"][vault]["strategies"].items():
+            flat = flatten_dict(strategy_params)
+            for key, value in flat.items():
+                if key in ["address", "version", "experimental"] or value is None:
+                    continue
+
+                label_values = _get_label_values(params, [vault, strategy, key], True)
+                label_names = mapping["v2_strategy"]["labels"]
+
+                item = _build_item(metric, label_names, label_values, value or 0, timestamp)
+                metrics_to_export.append(item)
+
+    # post all metrics for this timestamp at once
+    _post(metrics_to_export)
+
+
+def export_wallets(timestamp, data):
+    metrics_to_export = []
+    for key, value in data['agg_stats'].items():
+        if key == 'wallet balances usd':
+            for wallet, usd_bal in value.items():
+                label_names = ["param","wallet"]
+                label_values = ["balance usd",wallet]
+                item = _build_item("aggregate", label_names, label_values, usd_bal, timestamp)
+                metrics_to_export.append(item)
+            continue
+        label_names = ['param']
+        label_values = [key]
+        item = _build_item("aggregate", label_names, label_values, value, timestamp)
+        metrics_to_export.append(item)
+    for product in ['v1','v2']:
+        metric = mapping[product]["metric"]
+        for key, value in data[product].items():
+            if key in mapping[product]["agg_stats"]:
+                label_names = ['param']
+                label_values = [key]
+                item = _build_item(metric, label_names, label_values, value, timestamp)
+                metrics_to_export.append(item)
+                continue
+            elif key == "wallet balances usd":
+                for wallet, usd_bal in value.items():
+                    label_names = ["param","wallet"]
+                    label_values = ["balance usd",wallet]
+                    item = _build_item(metric, label_names, label_values, usd_bal, timestamp)
+                    metrics_to_export.append(item)
+                continue
+            
+            vault, params = key, value
+            for k, v in params.items():
+                if k == 'wallet balances':
+                    for wallet, bals in v.items():
+                        for denom, bal in bals.items():
+                            label_values = [wallet] + _get_label_values(params, [vault, denom], product in ['v2','special'])
+                            label_names = ["wallet"] + mapping[product]["labels"]
+                            item = _build_item(metric, label_names, label_values, bal, timestamp)
+                            metrics_to_export.append(item)
+                    continue
+
+                label_values = _get_label_values(params, [vault, k], True)
+                label_names = mapping[product]["labels"]
+
+                item = _build_item(metric, label_names, label_values, v, timestamp)
+                metrics_to_export.append(item)
+
+    # post all wallet metrics for this timestamp at once
+    _post(metrics_to_export)
+
+def export_treasury(timestamp, data):
+    metrics_to_export = []
+    for section, section_data in data.items():
+        for wallet, wallet_data in section_data.items():
+            for token, bals in wallet_data.items():
+                symbol = 'ETH' if token == 'ETH' else contract(token).symbol()
+                for key, value in bals.items():
+                    if value != 0:
+                        label_names = ['param','wallet','token_address','token','bucket']
+                        label_values = [key,wallet,token,symbol,get_token_bucket(token)]
+                        item = _build_item(f"treasury_{section}",label_names,label_values,value,timestamp)
+                        metrics_to_export.append(item)
+    
+    # post all metrics for this timestamp at once
+    _post(metrics_to_export)
+
+
+def export_duration(duration_seconds, pool_size, direction, timestamp_seconds):
+    item = _build_item(
+      "export_duration",
+      [ "pool_size", "direction" ],
+      [ pool_size, direction ],
+      duration_seconds,
+      timestamp_seconds
+    )
+    _post([item])
+
+
+def _build_item(metric, label_names, label_values, value, timestamp):
+    ts_millis = math.floor(timestamp) * 1000
+    label_names.append("network")
+    label_values.append(Network.label(chain.id))
+    meta = dict(zip(map(_sanitize, label_names), map(str, label_values)))
+    meta["__name__"] = metric
+    return {"metric": meta, "values": [_sanitize(value)], "timestamps": [ts_millis]}
+
+
+def _to_jsonl_gz(metrics_to_export: List[Dict]):
+    lines = []
+    for item in metrics_to_export:
+        lines.append(json.dumps(item))
+
+    jsonlines = "\n".join(lines)
+    return gzip.compress(bytes(jsonlines, "utf-8"))
+
+
+def _post(metrics_to_export: List[Dict]):
+    data = _to_jsonl_gz(metrics_to_export)
+    base_url = os.environ.get('VM_URL', 'http://victoria-metrics:8428')
+    url = f'{base_url}/api/v1/import'
+    headers = {
+        'Connection': 'close',
+        'Content-Encoding': 'gzip'
+    }
+    with requests.Session() as session:
+        session.post(
+            url = url,
+            data = data,
+            headers = headers
+        )
+
+
+def _sanitize(value):
+    if isinstance(value, bool):
+        return int(value)
+    elif isinstance(value, str):
+        return value.replace('"', '')  # e.g. '"yvrenBTC" 0.3.5 0x340832'
+    
+    return value
+
+
+def flatten_dict(d):
+    def items():
+        for key, value in d.items():
+            if isinstance(value, dict):
+                for subkey, subvalue in flatten_dict(value).items():
+                    yield key + "." + subkey, subvalue
+            else:
+                yield key, value
+
+    return dict(items())
+
+
+def _get_label_values(params, inital_labels, experimental = False):
+    address = _get_string_label(params, "address")
+    version = _get_string_label(params, "version")
+    label_values = inital_labels + [address, version]
+    if experimental:
+        experimental_label = _get_bool_label(params, "experimental")
+        label_values.append(experimental_label)
+
+    return label_values
+
+
+def _get_bool_label(a_dict, key):
+    return "true" if key in a_dict and a_dict[key] == True else "false"
+
+
+def _get_string_label(a_dict, key):
+    return str(a_dict[key]) if key in a_dict else "n/a"
--- yearn/yearn.py
+++ yearn/yearn.py
@@ -3,6 +3,7 @@ from collections import Counter
 from time import time
 
 from brownie import chain
+from collections import Counter
 from joblib import Parallel, delayed
 
 import yearn.iearn
@@ -29,7 +30,7 @@ class Yearn:
         if chain.id == Network.Mainnet:
             self.registries = {
                 "earn": yearn.iearn.Registry(),
-                "v1": yearn.v1.registry.Registry(),
+                "v1": yearn.v1.registry.Registry(watch_events_forever=watch_events_forever),
                 "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(exclude_ib_tvl=exclude_ib_tvl),
                 "special": yearn.special.Registry(),
@@ -53,6 +54,9 @@ class Yearn:
             self.registries["v2"].load_strategies()
         if load_harvests:
             self.registries["v2"].load_harvests()
+        if load_transfers:
+            self.registries['v1'].load_transfers()
+            self.registries['v2'].load_transfers()
         logger.info('loaded yearn in %.3fs', time() - start)
 
 
@@ -72,7 +76,24 @@ class Yearn:
             delayed(self.registries[key].describe)(block=block)
             for key in self.registries
         )
-        return dict(zip(self.registries, desc))
+        results_dict = dict(zip(self.registries, desc))
+        user_balances = Counter()
+        for registry in desc:
+            for vault, data in registry.items():
+                try:
+                    for user, bals in data['user balances'].items():
+                        user_balances[user] += bals["usd balance"]
+                except: # process vaults, not aggregated stats
+                    pass # TODO: add total users and user balances for earn, ib, special
+        print(len(user_balances))
+        agg_stats = {
+            "agg_stats": {
+                "total users": len(user_balances),
+                "user balances usd": user_balances
+            }
+        }
+        results_dict.update(agg_stats)
+        return results_dict
 
 
     def describe_wallets(self, block=None):
