042721c8f586248f05d968a81c4635f080aa266f
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-01-27 10:07:17 +0100

    fix: manually merge ib handling code into the PR branch.

--- yearn/outputs/victoria.py
+++ yearn/outputs/victoria.py
@@ -64,9 +64,16 @@ def export(block, timestamp, data):
                 label_values = _get_label_values(params, [vault, key], has_experiments)
                 label_names = mapping[product]["labels"]
 
-                item = _build_item(metric, label_names, label_values, value, timestamp)
-                metrics_to_export.append(item)
-
+                if product == "ib" and key == 'tvl' and block >= constants.ib_snapshot_block:
+                    # create one item with tvl=0 that will be used in existing dashboards
+                    item_legacy = _build_item(metric, label_names, label_values, 0, timestamp)
+                    metrics_to_export.append(item_legacy)
+                    # create a second item to track ib tvl separately
+                    item_own = _build_item(f'{metric}_own', label_names, label_values, value, timestamp)
+                    metrics_to_export.append(item_own)
+                else:
+                    item = _build_item(metric, label_names, label_values, value, timestamp)
+                    metrics_to_export.append(item)
 
     for vault, params in data["v2"].items():
         metric = mapping["v2"]["metric"]
