1219c7d4f752704b3bfffe344f1604f4d1dc7102
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-02-02 23:44:38 +0100

    fix: adding some CL price feed mappings for fantom from: https://data.chain.link/fantom/mainnet

--- yearn/prices/constants.py
+++ yearn/prices/constants.py
@@ -48,9 +48,12 @@ stablecoins_by_network = {
     Network.Fantom: {
         "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75": "usdc",
         "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E": "dai",
+        "0x1B27A9dE6a775F98aaA5B90B62a4e2A0B84DbDd9": "usdt",
+        "0xC931f61B1534EB21D8c11B24f3f5Ab2471d4aB50": "busd",
         "0xe2D27f06F63d98b8e11b38b5b08A75D0c8dD62B9": "ust",
         "0x82f0B8B456c1A451378467398982d4834b6829c1": "mim",
-        "0x049d68029688eAbF473097a2fC38ef61633A3C7A": "fusdt"
+        "0x049d68029688eAbF473097a2fC38ef61633A3C7A": "fusdt",
+        "0xdc301622e621166BD8E82f2cA0A26c13Ad0BE355": "frax",
     },
     Network.Arbitrum: {
         '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'usdc',
