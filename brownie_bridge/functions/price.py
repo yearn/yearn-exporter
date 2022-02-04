c98f2157c985ffe36d4c95b2547e9ce8e4526503
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-02-04 11:25:39 +0100

    feat: report any unknown exceptions to sentry while getting the price.

--- brownie_bridge/functions/price.py
+++ brownie_bridge/functions/price.py
@@ -1,5 +1,6 @@
 from yearn.prices import magic
 from yearn.exceptions import PriceError
+from sentry_sdk import capture_exception
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,5 +12,10 @@ def get_price(address):
     except (PriceError, AttributeError, ValueError) as e:
         logger.error(e)
         logger.error("Could not find price for %s", address)
+    except Exception as e:
+        capture_exception(e)
+        logger.error(e)
+        logger.error("Could not find price for %s", address)
+
 
     return { "address": address, "price": price, "unit": "USD" }
