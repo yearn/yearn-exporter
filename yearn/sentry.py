342bba3b5df196eb9a58ef9e2e4d8cd7033d79a2
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-02-08 08:55:17 +0100

    feat: using the default envs that sentry picks up by default: SENTRY_ENVIRONMENT, SENTRY_RELEASE.

--- yearn/sentry.py
+++ yearn/sentry.py
@@ -6,8 +6,6 @@ from sentry_sdk.integrations.threading import ThreadingIntegration
 
 from yearn.networks import Network
 
-environment = os.getenv('ENV', 'DEVELOPMENT')
-
 def before_send(event, hint):
     # custom event parsing goes here
     return event
@@ -29,7 +27,6 @@ def setup_sentry():
             # We recommend adjusting this value in production.
             traces_sample_rate=1.0,
             shutdown_timeout=5,
-            environment=environment,
             before_send=before_send,
             debug=False,
             integrations=[ThreadingIntegration(propagate_hub=True)],
