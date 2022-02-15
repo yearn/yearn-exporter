57abadba63b16a91191a7ae169f45b67a72a7401
0xkofee <koffee888@protonmail.com>
2022-02-15 15:43:16 -0600

    fix: reorganized imports so api file is able to run without brownie being initialized

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,7 +1,9 @@
+from brownie import network
 from yearn.logs import setup_logging
-from yearn.middleware.middleware import setup_middleware
 from yearn.sentry import setup_sentry
 
 setup_logging()
-setup_middleware()
 setup_sentry()
+if network.is_connected():
+    from yearn.middleware.middleware import setup_middleware
+    setup_middleware()
