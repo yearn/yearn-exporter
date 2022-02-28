8b3ec3ab4e2aace413b916c6eab4c3cd90e6f493
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-02-28 13:42:47 +0100

    feat: DRY entrypoint and add arbitrum specific network config.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,25 @@ if [[ $NETWORK == "mainnet" ]]; then
   CHAIN_ID=1
   EXPLORER=${EXPLORER:-https://api.etherscan.io/api}
 
+  # some legacy cleanup
   if [[ ! -z "$WEB3_PROVIDER" ]]; then
     if [[ $(brownie networks list | grep mainnet-custom) ]]; then
       brownie networks delete mainnet-custom
     fi
-    brownie networks modify mainnet host=$WEB3_PROVIDER chainid=$CHAIN_ID explorer=$EXPLORER
   fi
 
 elif [[ $NETWORK == "ftm-main" ]]; then
   CHAIN_ID=250
   EXPLORER=${EXPLORER:-https://api.ftmscan.com/api}
 
-  if [[ ! -z "$WEB3_PROVIDER" ]]; then
-    brownie networks modify ftm-main host=$WEB3_PROVIDER chainid=$CHAIN_ID explorer=$EXPLORER
-  fi
+elif [[ $NETWORK == "arbitrum-main" ]]; then
+  CHAIN_ID=42161
+  EXPLORER=${EXPLORER:-https://api.arbiscan.io/api}
+fi
+
+# modify the network
+if [[ ! -z "$WEB3_PROVIDER" ]]; then
+  brownie networks modify $NETWORK host=$WEB3_PROVIDER chainid=$CHAIN_ID explorer=$EXPLORER
 fi
 
 if [[ $# -eq 0 ]]; then
