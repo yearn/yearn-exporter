3d0bb35e38e3a9335ee9ddd51104fd197b203152
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-02-28 23:18:01 +0000

    fix: treasury exporter where token.symbol fails

--- yearn/outputs/victoria/output_treasury.py
+++ yearn/outputs/victoria/output_treasury.py
@@ -7,12 +7,21 @@ def export(timestamp, data, label):
     for section, section_data in data.items():
         for wallet, wallet_data in section_data.items():
             for token, bals in wallet_data.items():
-                symbol = 'ETH' if token == 'ETH' else contract(token).symbol()
+                symbol = _get_symbol(token)
+                bucket = get_token_bucket(token)
                 for key, value in bals.items():
                     label_names = ['param','wallet','token_address','token','bucket']
-                    label_values = [key,wallet,token,symbol,get_token_bucket(token)]
+                    label_values = [key,wallet,token,symbol,bucket]
                     item = _build_item(f"{label}_{section}",label_names,label_values,value,timestamp)
                     metrics_to_export.append(item)
     
     # post all metrics for this timestamp at once
     _post(metrics_to_export)
+
+def _get_symbol(token):
+    if token == 'ETH':
+        return 'ETH'
+    try:
+        return contract(token).symbol()
+    except AttributeError:
+        return None
