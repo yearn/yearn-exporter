e7c4a85c7d107e88c969206764e6a00368cab45b
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2022-03-01 21:59:22 +0000

    chore: black

--- scripts/treasury_exporter.py
+++ scripts/treasury_exporter.py
@@ -9,6 +9,7 @@ from yearn.outputs.victoria import output_duration
 logger = logging.getLogger('yearn.treasury_exporter')
 sleep_interval = int(os.environ.get('SLEEP_SECONDS', '0'))
 
+
 def main():
     treasury = YearnTreasury(watch_events_forever=True)
     for block in chain.new_blocks(height_buffer=12):
