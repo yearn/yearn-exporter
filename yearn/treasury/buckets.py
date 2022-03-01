ea51bbecf99f32c0c8f0906d64b96ec59c6dc6c0
Bob The Buidler <bobthebuidlerdefi@gmail.com>
2022-03-01 21:59:22 +0000

    feat: add buckets

--- yearn/treasury/buckets.py
+++ yearn/treasury/buckets.py
@@ -71,7 +71,6 @@ def get_token_bucket(token) -> str:
         return 'Other long term assets'
     return 'Other short term assets'
 
-
 @memory.cache()
 def _unwrap_token(token) -> str:
     '''
@@ -101,7 +100,6 @@ def _unwrap_token(token) -> str:
         except AttributeError:
             return weth
     return token
-
 
 def _pool_bucket(pool_tokens: set) -> str:
     if pool_tokens < BTC_LIKE:
