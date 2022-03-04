eb616974a68653fe547b4f58fdfee4e9ffdccf6c
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-03-04 21:18:24 +0100

    feat: adding export_mode to tg message

--- scripts/s3.py
+++ scripts/s3.py
@@ -257,9 +257,10 @@ def with_monitoring():
         main()
     except Exception as error:
         tb = traceback.format_exc()
+        export_mode = os.getenv("EXPORT_MODE", "endorsed")
         now = datetime.now()
         tags = " ".join(telegram_users_to_alert)
-        message = f"`[{now}]`\n🔥 API (vaults) update for {Network(chain.id).name} failed!\n```\n{tb}\n```\n{tags}"[:4000]
+        message = f"`[{now}]`\n🔥 API ({export_mode} vaults) update for {Network(chain.id).name} failed!\n```\n{tb}\n```\n{tags}"[:4000]
         updater.bot.send_message(chat_id=private_group, text=message, parse_mode="Markdown", reply_to_message_id=ping)
         updater.bot.send_message(chat_id=public_group, text=message, parse_mode="Markdown")
         raise error
