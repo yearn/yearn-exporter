7b07f122669179db79a40ede5ca63f194be76fa0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    fix(test): add fantom DAI and gDAI addresses for test_aave

--- tests/prices/test_aave.py
+++ tests/prices/test_aave.py
@@ -4,15 +4,18 @@ from yearn.networks import Network
 from yearn.prices.aave import aave
 
 aDAI = {
-    Network.Mainnet: '0x028171bCA77440897B824Ca71D1c56caC55b68A3',
+    Network.Mainnet:    '0x028171bCA77440897B824Ca71D1c56caC55b68A3',
+    Network.Fantom:     '0x07E6332dD090D287d3489245038daF987955DCFB', # gDAI
 }.get(chain.id, None)
 
 DAI = {
-    Network.Mainnet: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    Network.Mainnet:    '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    Network.Fantom:     '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
 }.get(chain.id, None)
 
 MIN_CT_MARKETS = {
     Network.Mainnet: 10,
+    Network.Fantom: 9,
 }.get(chain.id, None)
 
 
