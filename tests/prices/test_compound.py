eb8a01cc170f80d26470403020daca1bf5732fc3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    fix(test): get test_compound working again

--- tests/prices/test_compound.py
+++ tests/prices/test_compound.py
@@ -1,35 +1,41 @@
-from yearn.prices.compound import compound
+from typing import List
 
-def test_compound_pricing():
-    for comp in compound.compounds:
-        print(comp)
-        for market in comp.markets:
-            print(market)
-            if market.token == '0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0':
-                continue  # creth is broken
-            print(market.name)
-            print(f'undecimals = {market.under_decimals}')
-            print(f' cdecimals = {market.cdecimals}')
+import pytest
+from brownie import chain
+from yearn.networks import Network
+from yearn.prices.compound import Compound, CompoundMarket, compound
 
-            print(f'  exchange = {market.get_exchange_rate()}')
-            print(f'underlying = {market.get_underlying_price()}')
-            print(f'     price = {compound.get_price(market.token)}')
+comps: List[Compound] = [comp for comp in compound.compounds]
+markets: List[CompoundMarket] = [market for comp in comps for market in comp.markets]
+
+@pytest.mark.parametrize('market',markets)
+def test_compound_pricing(market):
+    if chain.id == Network.Mainnet and str(market.ctoken) == '0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0':
+        pytest.skip('creth is broken.')
+
+    print(market)
+    print(market.name)
+    print(f'undecimals = {market.under_decimals}')
+    print(f' cdecimals = {market.cdecimals}')
+    print(f'  exchange = {market.get_exchange_rate()}')
+    print(f'underlying = {market.get_underlying_price()}')
+    print(f'     price = {compound.get_price(str(market.ctoken))}')
 
 
-def test_compound_cap():
-    for comp in compound.compounds:
-        print(comp.name)
-        total = 0
-        for token in comp.markets:
-            if token.token == '0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0':
-                continue  # creth is broken
-            
-            price = comp.get_price(token.token)
-            if isinstance(price, list):
-                price = price[0]
+@pytest.mark.parametrize('compound',comps)
+def test_compound_cap(compound):
+    print(compound.name)
+    total = 0
+    for market in compound.markets:
+        if chain.id == Network.Mainnet and str(market.ctoken) == '0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0':
+            continue  # creth is broken
+        
+        price = compound.get_price(market.token)
+        if isinstance(price, list):
+            price = price[0]
 
-            supply = token.ctoken.totalSupply() / 10 ** token.cdecimals
-            print(f'  {token.name} {supply:,.0f} x {price:,.2f} = {supply * price:,.0f}')
-            total += supply * price
+        supply = market.ctoken.totalSupply() / 10 ** market.cdecimals
+        print(f'  {market.name} {supply:,.0f} x {price:,.2f} = {supply * price:,.0f}')
+        total += supply * price
 
-        print(f'{comp.name} = {total:,.0f}')
+    print(f'{compound.name} = {total:,.0f}')
