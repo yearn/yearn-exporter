9ea9ee4ab883b1083a253582bbf30cc7578c178e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    feat(test): adapt test_fixed_forex for multichain

--- tests/fixtures/decorators.py
+++ tests/fixtures/decorators.py
@@ -3,6 +3,7 @@ import pytest
 from brownie import chain
 from yearn.networks import Network
 from yearn.ironbank import addresses as ironbank_registries
+from yearn.prices.fixed_forex import addresses as ff_registries
 
 mainnet_only = pytest.mark.skipif(
     chain.id != Network.Mainnet,
@@ -12,4 +13,9 @@ mainnet_only = pytest.mark.skipif(
 ib_chains_only = pytest.mark.skipif(
     chain.id not in ironbank_registries,
     reason='Not applicable on chains without IronBank deployments.'
+)
+
+ff_chains_only = pytest.mark.skipif(
+    chain.id not in ff_registries,
+    reason='Not applicable on chains without Fixed Forex deployments.'
 )
