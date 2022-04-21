83891e9f63ecd536b5e75d1acf0af10b6c1138ab
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    chore(test): use new mainnet_only fixture for readability

--- tests/prices/test_synthetix.py
+++ tests/prices/test_synthetix.py
@@ -1,4 +1,5 @@
 import pytest
+from tests.fixtures.decorators import mainnet_only
 from yearn.prices.synthetix import synthetix
 from yearn.utils import contract
 
@@ -7,18 +8,18 @@ if synthetix:
     SYNTHS = synthetix.synths
 
 
-@pytest.mark.require_network('mainnet')
+@mainnet_only
 def test_get_synths():
     assert len(synthetix.synths) >= 10
 
 
-@pytest.mark.require_network('mainnet')
+@mainnet_only
 def test_synthetix_detection():
     sLINK = '0xbBC455cb4F1B9e4bFC4B73970d360c8f032EfEE6'
     assert sLINK in synthetix
 
 
-@pytest.mark.require_network('mainnet')
+@mainnet_only
 @pytest.mark.parametrize('target', SYNTHS)
 def test_synthetix_price(target):
     token = contract(target).proxy()
