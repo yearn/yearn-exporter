c1a71c0a6a44c8cbce1ec5732275be59c3709630
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    feat(test): adapt test_curve for multichain

--- tests/prices/test_curve.py
+++ tests/prices/test_curve.py
@@ -19,7 +19,7 @@ from yearn.utils import contract, contract_creation_block
 if chain.id == Network.Mainnet:
     pooldata = json.load(open('tests/fixtures/pooldata.json'))
 else:
-    pooldata = None
+    pooldata = {}
 
 registries = {
     Network.Mainnet: {
@@ -28,11 +28,15 @@ registries = {
     },
 }.get(chain.id, {})
 
-meta_factories = {
+# Contract(address_provider).get_address(3)
+meta_factories = { 
     Network.Mainnet: {
         'v1': '0x0959158b6040D32d04c301A72CBFD6b39E21c9AE',
         'v2': '0xB9fC157394Af804a3578134A6585C0dc9cc990d4',
     },
+    Network.Fantom: {
+        'v2': '0x686d67265703D1f124c45E33d47d794c566889Ba',
+    }
 }.get(chain.id, {})
 
 new_metapools = {
@@ -63,6 +67,103 @@ new_metapools = {
         "0x9c2C8910F113181783c249d8F6Aa41b51Cde0f0c",
         "0xc8a7C1c4B748970F57cA59326BcD49F5c9dc43E3",
         "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a",
+    ],
+    Network.Fantom: [
+        '0x03fa4c23bA3a7BC0BeC18789CE29c7857324bD22',
+        '0x075C1D1d7E9E1aF077B0b6117C6eA06CD0a260b5',
+        '0x0F5e1fC301437b344271c14Ffb52431a97CE4113',
+        '0x11093B0F9780ACcdb6322959a45d749Ca3789257',
+        '0x115266f936B2ea12eBb5cf5eB2D35f9c46614436',
+        '0x12cb0687B95158340287D8391D0Ca5314c10532D',
+        '0x138CCfE5d99c139B9c4918AcE02a082cA67693bB',
+        '0x1484Ae179657e01dE76C0Dc95A5838B1e797eB81',
+        '0x16E0bf935e696E0652CDB3EfA10b6A00C162d135',
+        '0x170eAAeBd5D398003A42A7414B704e13E1744c3a',
+        '0x1c7FA28E7BFa396a4B044cC939daFF51674d852B',
+        '0x1cb5C03d0585333A6D0578D47a5F9de66C80fD5A',
+        '0x1dc3F42f9E598999a784498D00f360F305A5B4D2',
+        '0x24699312CB27C26Cfc669459D670559E5E44EE60',
+        '0x259303442a9772222856C9Ae5212051048Bd3D9D',
+        '0x261036923F4aAF1322a3173921fCBdaE51352CAe',
+        '0x262a70f82df61a94F3E6fCD17895A25d6bEb07F6',
+        '0x27E611FD27b276ACbd5Ffd632E5eAEBEC9761E40',
+        '0x28368D7090421Ca544BC89799a2Ea8489306E3E5',
+        '0x2dd7C9371965472E5A5fD28fbE165007c61439E1',
+        '0x34033F0227C48bc3577aE5C6923A3DEEF3E36670',
+        '0x36586206bFc13ED520597b3DAE1313C8b1496388',
+        '0x38D0dD9d036ce6f291442c258F1cc2FC0d10e682',
+        '0x43EC4106b3A09b17f8669E268f36Cc5cdd5F356f',
+        '0x43a9f7d71F59DA647E367a713b2c2534A3938047',
+        '0x46F5ea3B7867b451Eb1568FA820a6e89c5492795',
+        '0x4cd4786426eD34DBd96ba64e4a7B676FAD25D71C',
+        '0x57F24a2E740516a351E2357d74215d235eE725FE',
+        '0x5AAC39FD59300f913096afafaa7b8E0be143f1b0',
+        '0x64a4CDc2c96146f8588dfB16DD3660Eb6Ea4109B',
+        '0x674E594A71464DDc55737ebfba37062B9044f9F4',
+        '0x68dde7344a302394845097E96E83cDdFe6D4d76e',
+        '0x6EF78ad4a40E9A6c81B9229de3eCc33ce591bC34',
+        '0x6d304d364B461Aab0496bf696A23F44a55fc43b6',
+        '0x7673F9226C8Ce29a86ED6Ad1C1D8574BB1d201e4',
+        '0x78AE5704B6664a07f14c7d3a14a98e7d6DfC80aa',
+        '0x7a656B342E14F745e2B164890E88017e27AE7320',
+        '0x7c79acC9aEf46E4d55BD05d06C24E79C35183241',
+        '0x7e4143Ed63635E5f53f0272c307eac4A5238037a',
+        '0x83A503780983bbaBd6ACab577FbA2c5A3DcAa57f',
+        '0x84f21b7ceb64906320E2b62cF73Ff03dcFd137dC',
+        '0x859009D02F0f609E585Be78e4427599FfaD4Ca14',
+        '0x872686B519E06B216EEf150dC4914f35672b0954',
+        '0x8B63F036F5a34226065bC0a7B0aE5bb5eBA1fF3D',
+        '0x8cD8Ab34189ee11A60c17Fb8bd394e4702De11fb',
+        '0x9171aaa40AD493fa6B6DACf60265272Cc58E49d0',
+        '0x92D5ebF3593a92888C25C0AbEF126583d4b5312E',
+        '0x9452699E70b648A3A499FA044bA0e58dEcB21A5a',
+        '0x96059756980fF6ced0473898d26F0EF828a59820',
+        '0x96817E9E47419dd66346AB64811e0f43aB75fEC8',
+        '0x9834766a92135DBd03b987Bf423aB89A18656194',
+        '0x9F83dbD9dBE351e3575a65dF04Ce34d3426e5B06',
+        '0x9a1394f9f9F2d600A4F4062190B941b036865FF1',
+        '0x9dc516a18775d492c9f061211C8a3FDCd476558d',
+        '0x9e85a45772F1078f3CaFf4B8c0FB43de4CA776E2',
+        '0xA58F16498c288c357e28EE899873fF2b55D7C437',
+        '0xA7CC236F81b04c1058e9bfb70E0Ee9940e271676',
+        '0xA7d2Ac1D5A1cFba7e3e21B68e76c48dd16348592',
+        '0xA8B3C9f298877dD93F30E8Ed359956faE10E8797',
+        '0xAD0FB83a110c3694faDa81e8B396716a610c4030',
+        '0xAD6D5548c45b1b198A4A563Cba1C3D384AFc44C6',
+        '0xB3045222d3F3725C4bEB1047f6C54514b3333ed7',
+        '0xB407481462ec40b706DF0D22e11d8BEFbc2Adf54',
+        '0xBe26e1858faFbF2de92fdd717AC098E34d9e526A',
+        '0xC1A2D9341135c7e3f00EDBED51F9C7acab7c7B3F',
+        '0xC822FC64AaD7b04B2eddE961c0704AB80F9D217B',
+        '0xCc0F443c62229f8944379d87a853f1898e682214',
+        '0xD5B51BA080F815446646a61039fBbFef5A12A4CF',
+        '0xD73a2802697263fb84cb06bBCe9501A2d33dc880',
+        '0xDC371bB71dE3a8E50683E7eb596690Db9D0365Cc',
+        '0xDE535Ee3999470286178B736e6A8D976a6822A3E',
+        '0xE8117F058A37367d6bf70a8E56B569d7fd5e2A8B',
+        '0xEEf1f7F11c3728100737c6355EEfD40Cacd6B506',
+        '0xEf3A4Fa40Fda4Eff051f14828dCe1C47d17488B3',
+        '0xF24f1061ee2C116D8dB1d0F6C791bCB741F9f08a',
+        '0xF427A0BE7524B022f89742EAf2F52EA433280aC3',
+        '0xF95ab97a4d41bA67A8F7298a3C9245C293855659',
+        '0xFa6Ddd52738FbEC9Ed649986AA1b74236B83aEbd',
+        '0xa1F70A0515BDc2954508125aDd49f72d01ee74c0',
+        '0xa3500EEC27bfD7d0000982DB3290a2c79143ed80',
+        '0xa5FAE68615010EFE0F22E98C48c75BFBB6141718',
+        '0xaD9c5054CC31f8AA822AEb9247298D2Ecf48c5cf',
+        '0xad71Fc10Fca8Ce6a98843A64b95E0C63516CA7F3',
+        '0xb17DeFAB9Da9A48F2712B92db8dA7c4EDb2D1c0a',
+        '0xbcC30798BBC4cDe6011a2C8cB7e5C21e5f0D0e35',
+        '0xbd05dF58eD93560457aD2Bc8d1Fe69Aa45Bbd636',
+        '0xc1ccCec6FC0D23B471E1c3877cb7558442D35Df5',
+        '0xc687e9038d7aCECF4842fD06D1b8d2d8CEF8e790',
+        '0xc87dc1E718E5dAbB438e69E8F3A1a6979dd46868',
+        '0xc955b79959aca2895AC8667A418849e4C218A708',
+        '0xd3f83ec4BB684712D9CA8519eDAfbb910470F77C',
+        '0xe1bA291eED6Ed14A4e65dCbe6a4936Fe15B3A1a4',
+        '0xe38e2c6361c14a17219f7c00bDD93DCc945f096A',
+        '0xeCf64ba816C75e595fF212391E80B5CD9071E7D5',
+        '0xee6Cc45B3c937DdBa9FAdD44eBA3Ea4dcceE05f1',
     ],
 }.get(chain.id, [])
 
@@ -179,10 +280,12 @@ old_metapools = {
 
 metapools = new_metapools + old_metapools
 
-eur_usd_crypto_pool_tokens = [
-    "0x3b6831c0077a1e44ED0a21841C3bC4dC11bCE833",
-    "0x3D229E1B4faab62F621eF2F6A610961f7BD7b23B"
-]
+eur_usd_crypto_pool_tokens = {
+    Network.Mainnet: [
+        "0x3b6831c0077a1e44ED0a21841C3bC4dC11bCE833",
+        "0x3D229E1B4faab62F621eF2F6A610961f7BD7b23B"
+    ],
+}.get(chain.id, [])
 
 
 @pytest.fixture(scope='session')
@@ -211,19 +314,26 @@ def test_curve_pool_from_lp_token(name):
     assert pool == pooldata[name]['swap_address']
 
 
-@pytest.mark.parametrize('pool', range(len(metapools)))
-def test_curve_metapool_from_lp_token(pool):
+@pytest.mark.parametrize('i', range(len(metapools)))
+def test_curve_metapool_from_lp_token(i):
     # metapool are both swap and lp
-    assert metapools[pool] == curve.curve.get_pool(metapools[pool])
+    pool = metapools[i]
+    assert pool == curve.curve.get_pool(pool)
 
 
 def test_curve_pool_from_lp_token_invalid():
     assert not curve.curve.get_pool('0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e')
 
 
-@pytest.mark.parametrize('pool', range(len(metapools)))
-def test_curve_registry_get_factory(pool):
-    assert curve.curve.get_factory(metapools[pool]) in meta_factories.values()
+@pytest.mark.parametrize('i', range(len(metapools)))
+def test_curve_registry_get_factory(i):
+    pool = metapools[i]
+    print(pool)
+    try:
+        assert curve.curve.get_factory(pool) in meta_factories.values()
+    except AssertionError:
+        # If we can't fetch factory, can we fetch registry?
+        assert curve.curve.get_registry(pool) == "0x0f854EA9F38ceA4B1c2FC79047E9D0134419D5d6"
 
 
 @pytest.mark.parametrize('name', pooldata)
@@ -242,14 +352,21 @@ def test_curve_gauge_convex(name, convex_gauge_map):
     assert convex_gauge == pooldata[name]['gauge_addresses'][0]
 
 
-@pytest.mark.parametrize('pool', range(len(new_metapools)))
-def test_curve_meta_gauge(pool):
-    if pool in [0, 4, 5, 6, 7, 9, 16, 17, 18, 24, 25]:
-        pytest.xfail('no gauge')
-    pool = new_metapools[pool]
-    gauge = curve.curve.get_gauge(pool)
-    print(pool, gauge)
-    assert gauge
+@pytest.mark.parametrize('i', range(len(new_metapools)))
+def test_curve_meta_gauge(i):
+    # NOTE This list might need updating once in a while if gauges are added for exiting pools.
+    no_gauge = {
+        Network.Mainnet: [0, 4, 5, 6, 7, 17, 18, 24],
+        Network.Fantom: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94]
+    }
+
+    pool = new_metapools[i]
+    if i in no_gauge[chain.id]:
+        assert not curve.curve.get_gauge(pool)
+    else:
+        gauge = curve.curve.get_gauge(pool)
+        print(pool, gauge)
+        assert gauge
 
 
 @pytest.mark.parametrize('name', pooldata)
@@ -315,9 +432,9 @@ def test_get_balances_fallback(name):
     print(name, curve.curve.get_balances(pool, block=registry_deploy))
 
 
-@pytest.mark.parametrize('pool', range(len(eur_usd_crypto_pool_tokens)))
-def test_crypto_pool_eur_usd_assets(pool):
-    lp_token = eur_usd_crypto_pool_tokens[pool]
+@pytest.mark.parametrize('i', range(len(eur_usd_crypto_pool_tokens)))
+def test_crypto_pool_eur_usd_assets(i):
+    lp_token = eur_usd_crypto_pool_tokens[i]
     pool = curve.curve.get_pool(lp_token)
     coins = curve.curve.get_coins(pool)
     non_zero_coins = list(filter(lambda coin: coin != ZERO_ADDRESS, coins))
