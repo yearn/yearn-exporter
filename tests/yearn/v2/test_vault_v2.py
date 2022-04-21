8b6e2734ea83ea8b12322c362a2c49e39e936e9c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    chore(test): refactor test_vault_v2

--- tests/yearn/v2/test_vault_v2.py
+++ tests/yearn/v2/test_vault_v2.py
@@ -20,34 +20,34 @@ def test_describe_vault_v2(vault: Vault):
     blocks = [randint(contract_creation_block(vault.vault.address), chain.height) for i in range(25)]
     for block in blocks:
         description = vault.describe(block=block) 
-        assert vault._views
-        for view in vault._views:
-            assert description[view]
-        assert description["token price"]
+        assert description["address"] == vault.vault.address, f'Incorrect address returned for {vault.name}.'
+        assert description["version"] == "v2", f'Incorrect version returned for {vault.name}.'
+
+        assert vault._views, f"Unable to fetch views for {vault.name}."
+        for param in vault._views + ["experimental", "strategies"]:
+            assert param in description, f"Unable to fetch {param} for {vault.name}."
+
         if 'totalAssets' in description:
-            assert description["tvl"]
-        
-        assert description["experimental"]
-        assert description["address"] == vault.vault.address
-        assert description["version"] == "v2"
-
-        assert description["strategies"]
-
+            assert "tvl" in description, f"Unable to fetch tvl for {vault.name}."
+                
+        assert description["strategies"], f"No strategies fetched for {vault.name}."
         for strategy in description["strategies"]:
-            assert strategy._views
+            strat_address = description["strategies"][strategy]["address"]
+            strategy = vault._strategies[strat_address]
+            assert strategy._views, f"Unable to fetch views for strategy {strategy.name}."
             for view in strategy._views:
-                assert description["strategies"][strategy][view]
+                assert view in description["strategies"][strategy], f"Unable to fetch {view} for strategy {strategy.name}."
 
 
 def test_active_vaults_at_v2_current():
-    assert registry.active_vaults_at() >= 91
+    assert len(registry.active_vaults_at()) >= 91, "One or more vaults are missing from v2.Registry().active_vaults_at(None)"
 
 
 @pytest.mark.parametrize('block',blocks)
 def test_active_vaults_at_v2(block):
-    assert registry.active_vaults_at(block=block)
+    assert registry.active_vaults_at(block=block), f"Unable to fetch active v2 vaults at block {block}."
 
 
 @pytest.mark.parametrize('block',blocks)
 def test_total_value_at_v2(block):
-    assert registry.total_value_at(block)
+    assert registry.total_value_at(block=block), f"Unable to fetch v2 tvl at block {block}."
