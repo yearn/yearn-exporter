f1445abfdee36d0fd4674a3d2cd1460a77da7d38
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-21 13:49:12 +0000

    feat(test): adapt test_aave for multichain

--- tests/fixtures/decorators.py
+++ tests/fixtures/decorators.py
@@ -2,20 +2,26 @@
 import pytest
 from brownie import chain
 from yearn.networks import Network
-from yearn.ironbank import addresses as ironbank_registries
-from yearn.prices.fixed_forex import addresses as ff_registries
+from yearn import ironbank
+from yearn.prices import fixed_forex
+from yearn.prices import aave
 
 mainnet_only = pytest.mark.skipif(
     chain.id != Network.Mainnet,
     reason="Only applicable on Mainnet."
 )
 
+aave_chains_only = pytest.mark.skipif(
+    chain.id not in aave.address_providers,
+    reason='Not applicable on chains without Aave deployments.'
+)
+
 ib_chains_only = pytest.mark.skipif(
-    chain.id not in ironbank_registries,
+    chain.id not in ironbank.addresses,
     reason='Not applicable on chains without IronBank deployments.'
 )
 
 ff_chains_only = pytest.mark.skipif(
-    chain.id not in ff_registries,
+    chain.id not in fixed_forex.addresses,
     reason='Not applicable on chains without Fixed Forex deployments.'
 )
--- tests/prices/test_aave.py
+++ tests/prices/test_aave.py
@@ -1,14 +1,28 @@
+from brownie import chain
+from tests.fixtures.decorators import aave_chains_only
+from yearn.networks import Network
 from yearn.prices.aave import aave
 
-aDAI = '0x028171bCA77440897B824Ca71D1c56caC55b68A3'
-DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+aDAI = {
+    Network.Mainnet: '0x028171bCA77440897B824Ca71D1c56caC55b68A3',
+}.get(chain.id, None)
+
+DAI = {
+    Network.Mainnet: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+}.get(chain.id, None)
+
+MIN_CT_MARKETS = {
+    Network.Mainnet: 10,
+}.get(chain.id, None)
 
 
+@aave_chains_only
 def test_aave():
     assert aDAI in aave
     assert aave.atoken_underlying(aDAI) == DAI
 
 
+@aave_chains_only
 def test_markets():
     assert aave.markets[aDAI] == DAI
-    assert len(aave.markets) > 10
+    assert len(aave.markets) > MIN_CT_MARKETS
