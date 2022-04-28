608ed5eb9d55017e20cef5143408bd1fce41e7fd
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-28 19:36:28 +0000

    feat: use deepest uniswap for price

--- yearn/prices/uniswap/uniswap.py
+++ yearn/prices/uniswap/uniswap.py
@@ -1,5 +1,5 @@
 
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, Optional, Union
 
 from yearn.prices.uniswap.v1 import UniswapV1, uniswap_v1
 from yearn.prices.uniswap.v2 import UniswapV2Multiplexer, uniswap_v2
@@ -7,29 +7,26 @@ from yearn.prices.uniswap.v3 import UniswapV3, uniswap_v3
 
 Uniswap = Union[UniswapV1,UniswapV2Multiplexer,UniswapV3]
 
+UNISWAPS: Dict[str,Optional[Uniswap]] = {
+    'v1': uniswap_v1,
+    'v2': uniswap_v2,
+    'v3': uniswap_v3
+}
+
 class UniswapVersionMultiplexer:
     def __init__(self) -> None:
-        self.uniswaps: Dict[str,Uniswap] = {
-            'v1': uniswap_v1,
-            'v2': uniswap_v2,
-            'v3': uniswap_v3
-        }
-        self.uniswaps = {version: uniswap for version, uniswap in self.uniswaps.items() if uniswap is not None}
-        self.try_order: List[str] = ['v2','v3','v1']
+        self.uniswaps: Dict[str,Uniswap] = {version: uniswap for version, uniswap in UNISWAPS.items() if uniswap is not None}
+    
+    def __contains__(self, token: Any) -> bool:
+        return len(self.uniswaps) > 0
 
-
-    def get_price(self, token: str, block: Optional[int]) -> Optional[float]:
-        for version in self.try_order:
-            if version not in self.uniswaps:
-                continue
-            price = self.uniswaps[version].get_price(token, block)
-            if price is None:
-                continue
-            return price
+    def get_price(self, token: str, block: Optional[int] = None) -> Optional[float]:
+        deepest_uniswap = self.deepest_uniswap(token, block)
+        if deepest_uniswap:
+            return deepest_uniswap.get_price(token, block=block)
         return None
 
-
-    def deepest_uniswap(self, token_in: str, block: Optional[int]) -> Optional[Uniswap]:
+    def deepest_uniswap(self, token_in: str, block: Optional[int] = None) -> Optional[Uniswap]:
         deepest_uniswap = None
         deepest_uniswap_balance = 0
         for uniswap in self.uniswaps.values():
