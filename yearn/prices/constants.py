5842f56742c12d504f221a734f7cec065713de65
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-28 19:36:28 +0000

    feat: use chainlink for anydai

--- yearn/prices/constants.py
+++ yearn/prices/constants.py
@@ -64,7 +64,8 @@ stablecoins_by_network = {
         "0x82f0B8B456c1A451378467398982d4834b6829c1": "mim",
         "0x049d68029688eAbF473097a2fC38ef61633A3C7A": "fusdt",
         "0xdc301622e621166BD8E82f2cA0A26c13Ad0BE355": "frax",
-        "0x95bf7E307BC1ab0BA38ae10fc27084bC36FcD605": "anyusdc"
+        "0x95bf7E307BC1ab0BA38ae10fc27084bC36FcD605": "anyusdc",
+        "0xd652776dE7Ad802be5EC7beBfafdA37600222B48": "anydai",
     },
     Network.Arbitrum: {
         '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'usdc',
