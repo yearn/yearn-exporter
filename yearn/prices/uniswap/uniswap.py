f8831b2fba32450028cccbf973e431c6c7de0ca2
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-28 19:36:28 +0000

    feat: bypass liquidity-based path selection for uniswap.get_price(weth)

--- yearn/prices/uniswap/uniswap.py
+++ yearn/prices/uniswap/uniswap.py
@@ -1,10 +1,14 @@
 
 from typing import Any, Dict, Optional, Union
 
+from brownie import convert
+from yearn.prices import constants
+from yearn.prices.chainlink import chainlink
 from yearn.prices.uniswap.v1 import UniswapV1, uniswap_v1
 from yearn.prices.uniswap.v2 import UniswapV2Multiplexer, uniswap_v2
 from yearn.prices.uniswap.v3 import UniswapV3, uniswap_v3
-from yearn.typing import Address, Block
+from yearn.typing import Address, AddressOrContract, Block
+from yearn.utils import contract, contract_creation_block
 
 Uniswap = Union[UniswapV1,UniswapV2Multiplexer,UniswapV3]
 
@@ -21,7 +25,13 @@ class UniswapVersionMultiplexer:
     def __contains__(self, token: Any) -> bool:
         return len(self.uniswaps) > 0
 
-    def get_price(self, token: Address, block: Optional[Block] = None) -> Optional[float]:
+    def get_price(self, token: AddressOrContract, block: Optional[Block] = None) -> Optional[float]:
+        token = convert.to_address(token)
+
+        # NOTE Following our usual logic with WETH is a big no-no. Too many calls.
+        if token == constants.weth and block <= contract_creation_block(chainlink.get_feed(token)):
+            return self._early_exit_for_eth(block=block)
+
         deepest_uniswap = self.deepest_uniswap(token, block)
         if deepest_uniswap:
             return deepest_uniswap.get_price(token, block=block)
@@ -36,5 +46,17 @@ class UniswapVersionMultiplexer:
                 deepest_uniswap = uniswap
                 deepest_uniswap_balance = deepest_pool_balance
         return deepest_uniswap
+    
+    def _early_exit_for_eth(self, block: Optional[Block] = None) -> Optional[float]:
+        ''' We use this to bypass the liquidity checker for ETH prior to deployment of the chainlink feed. '''
+        amount_in = 1e18
+        path = [constants.weth, constants.usdc]
+        for uni in self.uniswaps['v2'].uniswaps:
+            if uni.name != "uniswap":
+                continue
+            quote = uni.router.getAmountsOut(amount_in, path, block_identifier=block)[-1]
+            quote /= 10 ** contract(constants.usdc).decimals()
+            fees = 0.997 ** (len(path) - 1)
+            return quote / fees
 
 uniswaps = UniswapVersionMultiplexer()
