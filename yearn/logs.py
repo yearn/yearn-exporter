140df914587e92e37410c55190eeff18df0488e7
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-05-26 00:33:44 +0200

    feat: pass LOG_LEVEL via env.

--- yearn/logs.py
+++ yearn/logs.py
@@ -5,7 +5,7 @@ from brownie.exceptions import BrownieEnvironmentWarning
 
 def setup_logging():
     logging.basicConfig(
-        level=logging.INFO,
+        level=os.getenv("LOG_LEVEL", "INFO"),
         format="%(levelname)s %(name)s:%(lineno)d %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
