2ef434e43ece2fe91e12e20d637cd1123f63c29e
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-06-21 21:11:11 +0200

    fix: price calculation for balancer v2 pool tokens.

--- yearn/prices/balancer/v2.py
+++ yearn/prices/balancer/v2.py
@@ -32,21 +32,21 @@ class BalancerV2(metaclass=Singleton):
     def get_version(self) -> str:
         return "v2"
 
-    def get_tokens(self, token: Address) -> List:
+    def get_tokens(self, token: Address, block: Optional[Block] = None) -> List:
         pool = contract(token)
         pool_id = pool.getPoolId()
         vault = contract(pool.getVault())
-        return vault.getPoolTokens(pool_id)[0]
+        return vault.getPoolTokens(pool_id, block_identifier=block)[0]
 
     @ttl_cache(ttl=600)
     def get_price(self, token: Address, block: Optional[Block] = None) -> float:
         pool = contract(token)
         pool_id = pool.getPoolId()
         vault = contract(pool.getVault())
-        tokens, supply = fetch_multicall([vault, "getPoolTokens", pool_id], [pool, "totalSupply"], block=block)
-        supply = supply / 1e18
-        balances = [balance / 10 ** contract(token).decimals() for token, balance in zip(tokens[0], tokens[1])]
-        total = sum(balance * magic.get_price(token, block=block) for token, balance in zip(tokens[0], tokens[1]))
+        tokens = vault.getPoolTokens(pool_id, block_identifier=block)
+        balances = [balance for t, balance in zip(tokens[0], tokens[1]) if t != token]
+        total = sum(balance * magic.get_price(t, block=block) for t, balance in zip(tokens[0], tokens[1]) if t != token)
+        supply = sum(balances)
         return total / supply
 
 balancer_v2 = None
