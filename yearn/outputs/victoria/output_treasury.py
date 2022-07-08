2b9dad1680851cff5b83db933ee37e497ff4bebb
borrito <30872891+kyhoon@users.noreply.github.com>
2022-07-08 16:20:46 +0200

    Add partner payout data to victoria-metrics (#349)
    * wip: add current progress
    
    * feat: export partner fees
    
    * refactor: remove unused packages
    
    * refactor: change gauge name to partners
    
    * fix: add additional guards for when the df is empty
    
    * fix: disable postgres cache to prevent integrity errors

--- yearn/outputs/victoria/output_treasury.py
+++ yearn/outputs/victoria/output_treasury.py
@@ -4,7 +4,13 @@ from yearn.utils import contract
 
 def export(timestamp, data, label):
     metrics_to_export = []
+
     for section, section_data in data.items():
+        # handle partners data
+        if section == 'partners':
+            export_partners(timestamp, section_data, label)
+            continue
+
         for wallet, wallet_data in section_data.items():
             for token, bals in wallet_data.items():
                 symbol = _get_symbol(token)
@@ -25,3 +31,54 @@ def _get_symbol(token):
         return contract(token).symbol()
     except AttributeError:
         return None
+
+
+def export_partners(timestamp, data, label):
+    metrics_to_export = []
+    label_names = ['partner', 'param', 'token_address', 'token', 'bucket']
+    for partner, partner_data in data.items():
+        for wrapper, wrapper_data in partner_data.items():
+            # get vault token
+            token = wrapper_data.get('vault')
+            if token is None:
+                continue
+            symbol = _get_symbol(token)
+            bucket = get_token_bucket(token)
+
+            # wrapper balance
+            for key in ['balance', 'balance_usd']:
+                item = _build_item(
+                    "partners",
+                    label_names,
+                    [partner, key, token, symbol, bucket],
+                    wrapper_data.get(key, 0.0),
+                    timestamp
+                )
+                metrics_to_export.append(item)
+
+            # payouts
+            payout = wrapper_data.get('payout', {})
+            for interval, value in payout.items():
+                key = f"payout_{interval}"
+                item = _build_item(
+                    "partners",
+                    label_names,
+                    [partner, key, token, symbol, bucket],
+                    value,
+                    timestamp
+                )
+                metrics_to_export.append(item)
+            payout_usd = wrapper_data.get('payout_usd', {})
+            for interval, value in payout_usd.items():
+                key = f"payout_usd_{interval}"
+                item = _build_item(
+                    "partners",
+                    label_names,
+                    [partner, key, token, symbol, bucket],
+                    value,
+                    timestamp
+                )
+                metrics_to_export.append(item)
+
+    # post all metrics for this timestamp at once
+    _post(metrics_to_export)
