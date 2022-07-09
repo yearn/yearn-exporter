a0db66ab8f27a6688a763ff91c884fc5e1edae4b
0xkofee <koffee888@protonmail.com>
2022-07-09 05:02:53 +0000

    feat: replace victoria with promscale

--- yearn/outputs/victoria/output_helper.py
+++ yearn/outputs/victoria/output_helper.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 import requests
 import os
 import gzip
@@ -46,7 +47,9 @@ def _build_item(metric, label_names, label_values, value, timestamp):
     label_values.append(Network.label(chain.id))
     meta = dict(zip(map(_sanitize, label_names), map(str, label_values)))
     meta["__name__"] = metric
-    return {"metric": meta, "values": [_sanitize(value)], "timestamps": [ts_millis]}
+    # "values": [_sanitize(value)], "timestamps": [ts_millis]
+    return {"labels": meta, "samples": [[ts_millis, _sanitize(value)]]}
+
 
 
 def _to_jsonl_gz(metrics_to_export: List[Dict]):
@@ -55,23 +58,29 @@ def _to_jsonl_gz(metrics_to_export: List[Dict]):
         lines.append(json.dumps(item))
 
     jsonlines = "\n".join(lines)
+    logger.info("JSONJSONJSON")
+    logger.info(jsonlines)
     return gzip.compress(bytes(jsonlines, "utf-8"))
 
 
 def _post(metrics_to_export: List[Dict]):
-    data = _to_jsonl_gz(metrics_to_export)
-    base_url = os.environ.get('VM_URL', 'http://victoria-metrics:8428')
-    url = f'{base_url}/api/v1/import'
+    # data = _to_jsonl_gz(metrics_to_export)
+    # TODO 
+    base_url = os.environ.get('PROMSCALE_URL', 'http://victoria-metrics:8428')
+    url = f'{base_url}/write'
     headers = {
-        'Connection': 'close',
-        'Content-Encoding': 'gzip'
+        'Content-Type': 'application/json'
     }
+    metrics_new = [json.dumps(metric) for metric in metrics_to_export]
+    logger.info("_post ---------------------------")
+    logger.info("".join(metrics_new))
     with requests.Session() as session:
-        session.post(
+        r = session.post(
             url = url,
-            data = data,
+            data = "".join(metrics_new), # remove list bracket and commas
             headers = headers
         )
+        logger.info(r)
 
 
 def _sanitize(value):
