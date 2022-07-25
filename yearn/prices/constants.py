8d714fc3b4f1c816cae8a183256d0587d1de4c9d
Dark Ghosty <38810205+DarkGhost7@users.noreply.github.com>
2022-07-25 14:06:46 -0400

    add: chainlink feeds

--- yearn/prices/constants.py
+++ yearn/prices/constants.py
@@ -71,6 +71,7 @@ stablecoins_by_network = {
         '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'usdc',
         '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1': 'dai',
         '0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A': 'mim',
+        '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9': 'usdt'
     },
 }
 
