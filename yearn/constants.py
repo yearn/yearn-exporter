2e67e85a7068d81930cc845f4b45774cbde3f656
Dark Ghosty <38810205+DarkGhost7@users.noreply.github.com>
2022-08-12 13:17:05 -0400

    feat: add ychad

--- yearn/constants.py
+++ yearn/constants.py
@@ -42,6 +42,7 @@ YCHAD_MULTISIG = {
     Network.Fantom:     "0xC0E2830724C946a6748dDFE09753613cd38f6767",
     Network.Gnosis:     "0x22eAe41c7Da367b9a15e942EB6227DF849Bb498C",
     Network.Arbitrum:   "0xb6bc033d34733329971b938fef32fad7e98e56ad",
+    Network.Optimism:   "0xF5d9D6133b698cE29567a90Ab35CfB874204B3A7",
 }.get(chain.id, None)
 
 if YCHAD_MULTISIG:
