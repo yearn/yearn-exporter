37695541ca9bb254b0c864b6557042c5fb7dd838
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-08-18 18:32:40 +0000

    feat: sort inbound anyswap bridge txs

--- yearn/treasury/accountant/ignore/__init__.py
+++ yearn/treasury/accountant/ignore/__init__.py
@@ -56,7 +56,7 @@ def is_kp3r(tx: TreasuryTx) -> bool:
         return False
 
 def is_bridged(tx: TreasuryTx) -> bool:
-
+    """ Cross-chain bridging """
     # Anyswap out - anyToken part
     if tx._symbol and tx._symbol.startswith("any") and "LogAnySwapOut" in tx._events:
         for event in tx._events["LogAnySwapOut"]:
@@ -68,7 +68,21 @@ def is_bridged(tx: TreasuryTx) -> bool:
     elif tx.to_address and tx.to_address.token and tx.to_address.token.symbol and tx.to_address.token.symbol.startswith("any") and "LogAnySwapOut" in tx._events:
         for event in tx._events["LogAnySwapOut"]:
             token, sender, receiver, amount, from_chainid, to_chainid = event.values()
-            if from_chainid == chain.id and tx.token.address.address == contract(token).underlying() and Decimal(amount) / tx.token.scale == tx.amount:
+            if from_chainid == chain.id and sender == tx.from_address.address and tx.token.address.address == contract(token).underlying() and Decimal(amount) / tx.token.scale == tx.amount:
+                return True
+    
+    # Anyswap in - anyToken part
+    elif tx._symbol and tx._symbol.startswith("any") and "LogAnySwapIn" in tx._events:
+        for event in tx._events["LogAnySwapIn"]:
+            txhash, token, receiver, amount, from_chainid, to_chainid = event.values()
+            if to_chainid == chain.id and tx.token.address.address == token and Decimal(amount) / tx.token.scale == tx.amount:
+                return True
+    
+    # Anyswap in - token part
+    elif tx.from_address and tx.from_address.token and tx.from_address.token.symbol and tx.from_address.token.symbol.startswith("any") and "LogAnySwapIn" in tx._events:
+        for event in tx._events["LogAnySwapIn"]:
+            txhash, token, receiver, amount, from_chainid, to_chainid = event.values()
+            if to_chainid == chain.id and receiver == tx.to_address.address and tx.token.address.address == contract(token).underlying() and Decimal(amount) / tx.token.scale == tx.amount:
                 return True
 
     return tx in HashMatcher({
