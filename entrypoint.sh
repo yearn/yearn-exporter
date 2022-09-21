a0b414136bfd7a22b257fb4839cf5398c9356c05
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-09-21 17:25:07 +0200

    feat: simplify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,31 +4,9 @@ set -e
 NETWORK=${NETWORK:-mainnet} # default to Ethereum mainnet
 EXPLORER=${EXPLORER:-$DEFAULT_EXPLORER}
 
-if [[ $NETWORK == "mainnet" ]]; then
-  CHAIN_ID=1
-  # some legacy cleanup
-  if [[ ! -z "$WEB3_PROVIDER" ]]; then
-    if [[ $(brownie networks list | grep mainnet-custom) ]]; then
-      brownie networks delete mainnet-custom
-    fi
-  fi
-
-elif [[ $NETWORK == "xdai-main" ]]; then
-  CHAIN_ID=100
-
-elif [[ $NETWORK == "ftm-main" ]]; then
-  CHAIN_ID=250
-
-elif [[ $NETWORK == "arbitrum-main" ]]; then
-  CHAIN_ID=42161
-
-elif [[ $NETWORK == "optimism-main" ]]; then
-  CHAIN_ID=10
-fi
-
 # modify the network
 if [[ ! -z "$WEB3_PROVIDER" ]]; then
-  brownie networks modify $NETWORK host=$WEB3_PROVIDER chainid=$CHAIN_ID explorer=$EXPLORER
+  brownie networks modify $NETWORK host=$WEB3_PROVIDER explorer=$EXPLORER
 fi
 
 if [[ $# -eq 0 ]]; then
