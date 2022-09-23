4b6a24c6c4324c6b20c8a9a312cc695b838c21b4
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-09-23 14:29:10 +0200

    feat: pretty print debug info

--- yearn/debug.py
+++ yearn/debug.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from pprint import pformat
 logger = logging.getLogger(__name__)
 
 class Debug(object):
@@ -7,4 +8,4 @@ class Debug(object):
     @staticmethod
     def extract_variables(variables):
       logger.info("*** DEBUG LOCAL VARIABLES ***")
-      return { k: v for k, v in variables.items() if '__' not in k and 'pdb' not in k and 'self' not in k }
+      return pformat({ k: v for k, v in variables.items() if '__' not in k and 'pdb' not in k and 'self' not in k })
