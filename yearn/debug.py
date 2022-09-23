77c70bace5327a1ae20754b7b2522360671462a9
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-09-23 16:17:55 +0200

    feat: track line_numbers for the collected variables as well.

--- yearn/debug.py
+++ yearn/debug.py
@@ -1,6 +1,6 @@
 import os
-import inspect
 import logging
+from inspect import getframeinfo, stack
 from yearn.utils import Singleton
 
 logger = logging.getLogger(__name__)
@@ -10,11 +10,13 @@ class Debug(metaclass=Singleton):
         self._collected_variables = {}
 
     def collect_variables(self, variables):
-        stack = inspect.stack()
-        class_name = stack[1][0].f_locals["self"].__class__.__name__
-        method_name = stack[1][0].f_code.co_name
+        caller = stack()[1][0]
+        caller_frame = getframeinfo(caller)
+        class_name = caller.f_locals["self"].__class__.__name__
+        method_name = caller.f_code.co_name
+        line_number = caller_frame.lineno
         extracted = { k: v for k, v in variables.items() if '__' not in k and 'pdb' not in k }
-        self._collected_variables[f'{class_name}#{method_name}'] = extracted
+        self._collected_variables[f'{class_name}#{method_name}:{line_number}'] = extracted
         return extracted
 
     def get_collected_variables(self):
