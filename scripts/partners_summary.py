c439b82df976a4353b9b3ef58215d51034dd5255
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-09-29 23:01:59 +0200

    feat: renamed partners script to partners_summary and added two Makefile recipes for running the summary script and the exporters.

--- scripts/partners_summary.py
+++ scripts/partners_summary.py
@@ -2,7 +2,7 @@ import sentry_sdk
 from yearn.partners.partners import partners
 from yearn.partners.snapshot import process_partners
 
-sentry_sdk.set_tag('script','partners')
+sentry_sdk.set_tag('script','partners_summary')
 
 
 def main():
