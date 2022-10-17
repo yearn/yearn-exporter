8a8e01195a0352d11c7c53c92ba8363c70265fd2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-10-17 17:17:44 -0400

    fix: non-verified Philippine Peso stablecoin

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,4 +1,5 @@
 from brownie import network, chain, Contract
+from y import Contract_erc20
 from yearn.networks import Network
 from yearn.logs import setup_logging
 from yearn.sentry import setup_sentry
@@ -13,3 +14,6 @@ if network.is_connected():
     if chain.id == Network.Mainnet:
         # compile LINK contract locally for mainnet with latest solc because the etherscan abi crashes event parsing
         Contract.from_explorer("0x514910771AF9Ca656af840dff83E8264EcF986CA")
+    elif chain.id == Network.Arbitrum:
+        # PHP Philippine Peso stablecoin is not verified. Force init it with ERC20 abi.
+        Contract_erc20("0xFa247d0D55a324ca19985577a2cDcFC383D87953")
