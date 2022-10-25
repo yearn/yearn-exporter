42646b5a8947f474392cd59230d0739366832198
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-10-25 10:16:08 -0400

    Fix peso (#415)
    * fix: non-verified Philippine Peso stablecoin
    
    * feat: add philippine peso on Arbi to intl stablecoins constant
    
    * feat: add ypricemagic to deps

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,5 +1,6 @@
 import json
 from brownie import network, chain, Contract
+from y import Contract_erc20
 from yearn.networks import Network
 from yearn.logs import setup_logging
 from yearn.sentry import setup_sentry
@@ -14,8 +15,10 @@ if network.is_connected():
     if chain.id == Network.Mainnet:
         # compile LINK contract locally for mainnet with latest solc because the etherscan abi crashes event parsing
         Contract.from_explorer("0x514910771AF9Ca656af840dff83E8264EcF986CA")
-
     elif chain.id == Network.Arbitrum:
+        # PHP Philippine Peso stablecoin is not verified. Force init it with ERC20 abi.
+        Contract_erc20("0xFa247d0D55a324ca19985577a2cDcFC383D87953")
+        
         # workaround for issues loading the partner tracker contract on arbitrum
         Contract.from_abi(
             name='YearnPartnerTracker',
