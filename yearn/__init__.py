9f3117b14ac9b4df37578ec7f36b5df0ab8e5a0a
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-10-25 11:22:44 +0200

    chore: move workaround to global init

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,3 +1,4 @@
+import json
 from brownie import network, chain, Contract
 from yearn.networks import Network
 from yearn.logs import setup_logging
@@ -13,3 +14,11 @@ if network.is_connected():
     if chain.id == Network.Mainnet:
         # compile LINK contract locally for mainnet with latest solc because the etherscan abi crashes event parsing
         Contract.from_explorer("0x514910771AF9Ca656af840dff83E8264EcF986CA")
+
+    elif chain.id == Network.Arbitrum:
+        # workaround for issues loading the partner tracker contract on arbitrum
+        Contract.from_abi(
+            name='YearnPartnerTracker',
+            address='0x0e5b46E4b2a05fd53F5a4cD974eb98a9a613bcb7',
+            abi=json.load(open('interfaces/yearn/partner_tracker_arbitrum.json'))
+        )
