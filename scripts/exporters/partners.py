9c013ce9e4227393b4251d6c1f1fad68413ef4c7
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-12-08 23:22:59 +0000

    feat: use eth_portfolio token bucket

--- scripts/exporters/partners.py
+++ scripts/exporters/partners.py
@@ -3,13 +3,13 @@ from typing import List, Tuple
 
 import sentry_sdk
 from brownie import chain
+from eth_portfolio.buckets import get_token_bucket
 from pandas import DataFrame
 from y.networks import Network
 
 from yearn.helpers.exporter import Exporter
 from yearn.outputs.victoria.victoria import _build_item, _post
 from yearn.partners.partners import partners
-from yearn.treasury.buckets import get_token_bucket
 from yearn.treasury.treasury import _get_symbol
 
 sentry_sdk.set_tag('script','partners_exporter')
