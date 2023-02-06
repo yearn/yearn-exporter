a29277ddbf891a3775953122445aa4d3d4640d97
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-02-06 14:26:07 +0000

    fix: caching

--- yearn/outputs/describers/registry.py
+++ yearn/outputs/describers/registry.py
@@ -12,16 +12,16 @@ class RegistryWalletDescriber:
     def __init__(self):
         self.vault_describer = VaultWalletDescriber()
 
-    def active_vaults_at(self, registry: tuple, block=None):
+    async def active_vaults_at(self, registry: tuple, block=None):
         label, registry = registry
-        active = [vault for vault in registry.active_vaults_at(block=block)]  
+        active = [vault for vault in await registry.active_vaults_at(block=block)]  
         if chain.id == Network.Mainnet:
             # [yGov] Doesn't count for this context
             active = [vault for vault in active if vault.vault != contract("0xBa37B002AbaFDd8E89a1995dA52740bbC013D992")]
         return active
         
     async def describe_wallets(self, registry: tuple, block=None):
-        active_vaults = self.active_vaults_at(registry, block=block)
+        active_vaults = await self.active_vaults_at(registry, block=block)
         data = await asyncio.gather(*[self.vault_describer.describe_wallets(vault.vault.address, block=block) for vault in active_vaults])
         data = {vault.name: desc for vault,desc in zip(active_vaults,data)}
 
