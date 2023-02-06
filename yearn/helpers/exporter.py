a6ce184fb8620c2e8bba743cb78737bdf23bbd50
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-02-06 14:26:07 +0000

    chore: bump export semaphore + 1

--- yearn/helpers/exporter.py
+++ yearn/helpers/exporter.py
@@ -88,7 +88,7 @@ class Exporter:
         self._data_semaphore = asyncio.Semaphore(self._concurrency)
         self._res_semaphore = defaultdict(lambda: asyncio.Semaphore(max_concurrent_runs_per_resolution))
         self._has_data_semaphore = asyncio.Semaphore(2)
-        self._export_semaphore = asyncio.Semaphore(1)
+        self._export_semaphore = asyncio.Semaphore(2)
 
         self._snapshots_fetched = 0
         self._snapshots_exported = 0
