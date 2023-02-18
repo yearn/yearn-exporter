19e6b2a2b4833ab62af2d49cf09241f12ca1f731
murderteeth <murdertxxth@gmail.com>
2023-02-18 20:47:15 +0000

    Make debug_apy functions callable from command line
    * Eg, this enables `brownie run scripts/debug_apy.py with_exception_handling`
    
    * Measure `vault.apy()` run time

--- scripts/debug_apy.py
+++ scripts/debug_apy.py
@@ -1,14 +1,18 @@
 import os
+import sys
+import time
 import logging
 import traceback
 
 logger = logging.getLogger(__name__)
 
 def main(address):
+    start = time.perf_counter()
     from yearn.v2.vaults import Vault
     from yearn.apy.common import get_samples
     vault = Vault.from_address(address)
     vault.apy(get_samples())
+    logger.info(f' ⏱️  {time.perf_counter() - start} seconds')
 
 def with_exception_handling():
     address = os.getenv("DEBUG_ADDRESS", None)
@@ -26,3 +30,6 @@ def with_exception_handling():
         logger.info("*** Available variables for debugging ***")
         available_variables = [ k for k in locals().keys() if '__' not in k and 'pdb' not in k and 'self' != k and 'sys' != k ]
         logger.info(available_variables)
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]]()
