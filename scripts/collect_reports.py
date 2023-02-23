938b9efddfdca148f9fe0b1f1dbcaa21639f8c98
wavey0x <retrowaveyy@gmail.com>
2023-02-22 20:23:15 -0500

    feat: sdcrv

--- scripts/collect_reports.py
+++ scripts/collect_reports.py
@@ -311,10 +311,14 @@ def handle_event(event, multi_harvest):
     r.token_symbol = get_contract(strategy.want()).symbol()
     r.want_token = strategy.want()
     r.want_price_at_block = 0
-    if r.want_token == "0x447Ddd4960d9fdBF6af9a790560d0AF76795CB08":
+    print(f'Want token = {r.want_token}')
+    if r.want_token == "0x447Ddd4960d9fdBF6af9a790560d0AF76795CB08": # Rocket Pool ETH
         r.want_price_at_block = magic.get_price(constants.weth, r.block) * get_contract(get_contract(r.want_token).coins(0)).getExchangeRate() / 1e18
-    elif r.want_token == "0x9848482da3Ee3076165ce6497eDA906E66bB85C5":
+    elif (r.want_token == "0x9848482da3Ee3076165ce6497eDA906E66bB85C5" or
+        r.want_token == "0x836A808d4828586A69364065A1e064609F5078c7"): # pETH
         r.want_price_at_block = magic.get_price(constants.weth, r.block)
+    elif r.want_token == '0xf7b55C3732aD8b2c2dA7c24f30A69f55c54FB717':
+        r.want_price_at_block = magic.get_price(crv, r.block)
     else:
         r.want_price_at_block = magic.get_price(r.want_token, r.block)
     
