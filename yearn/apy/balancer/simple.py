668766ec1de651ddf77877d2d59f67769bcdf3ad
murderteeth <murdertxxth@gmail.com>
2023-02-26 09:10:18 +0000

    Calculate aura current apr instead of projected

--- yearn/apy/balancer/simple.py
+++ yearn/apy/balancer/simple.py
@@ -2,11 +2,11 @@ import logging
 import os
 from pprint import pformat
 
-from brownie import chain
+from brownie import chain, web3
 from dataclasses import dataclass
 from semantic_version import Version
 
-from yearn.apy.common import Apy, ApyBlocks, ApyError, ApyFees, ApySamples, calculate_pool_apy
+from yearn.apy.common import Apy, ApyBlocks, ApyError, ApyFees, ApySamples, calculate_pool_apy, SECONDS_PER_YEAR
 from yearn.apy.booster import get_booster_fee, get_booster_reward_apr
 from yearn.apy.gauge import Gauge
 from yearn.debug import Debug
@@ -18,13 +18,14 @@ from yearn.utils import contract
 logger = logging.getLogger(__name__)
 
 @dataclass
-class AuraApyData:
+class AuraAprData:
     boost: float = 0
-    apr: float = 0
-    apr_minus_keep: float = 0
-    keep: float = 0
+    bal_apr: float = 0
+    aura_apr: float = 0
+    extra_rewards_apr: float = 0
+    gross_apr: float = 0
+    net_apr: float = 0
     debt_ratio: float = 0
-    reward_apr: float = 0
 
 addresses = {
     Network.Mainnet: {
@@ -71,163 +72,98 @@ def simple(vault, samples: ApySamples) -> Apy:
     )
 
 def calculate_simple(vault, gauge: Gauge, samples: ApySamples) -> Apy:
+    if not vault: raise ApyError('bal', 'apy preview not supported')
+
     now = samples.now
     pool_price_per_share = gauge.pool.getRate(block_identifier=now)
     pool_token_price = magic.get_price(gauge.lp_token, block=now)
-    bal_price = magic.get_price(addresses[chain.id]['bal'], block=now)
-    gauge_base_apr = gauge.calculate_base_apr(MAX_BOOST, bal_price, pool_price_per_share, pool_token_price)
-    _, pool_apy = calculate_pool_apy(vault, gauge.pool.getRate, samples)
-    pool_rewards_apr = gauge.calculate_rewards_apr(pool_price_per_share, pool_token_price, block=now)
-
-    if vault:
-        return calculate_simple_aura_apy(
-            vault, gauge, 
-            pool_rewards_apr,
-            pool_price_per_share, 
-            pool_token_price, 
-            gauge_base_apr, 
-            pool_apy,
-            samples
-        )
-
-    # currently just for apy preview, mirrors corresponding code from crv-cvx calculation
-    # assumes a 2 strategy design, one for balancer with 100% dr, one for aura with 0 dr
-    performance_fee, management_fee, keep_bal = get_vault_fees(vault, block=now)
-    voter_boost = gauge.calculate_boost(MAX_BOOST, addresses[chain.id]['voter'], block=now)
-    aura_apy_data = AuraApyData(debt_ratio=0)
-    bal_debt_ratio = 1
-
-    bal_apr = gauge_base_apr * voter_boost + pool_rewards_apr
-    bal_apr_minus_keep_bal = gauge_base_apr * voter_boost * (1 - keep_bal)
-
-    gross_apr = (1 + (bal_apr * bal_debt_ratio + aura_apy_data.apr * aura_apy_data.debt_ratio)) * (1 + pool_apy) - 1
-
-    aura_net_apr = (aura_apy_data.apr_minus_keep + aura_apy_data.reward_apr) * (1 - performance_fee) - management_fee
-    aura_net_farmed_apy = (1 + (aura_net_apr / COMPOUNDING)) ** COMPOUNDING - 1
-    aura_net_apy = ((1 + aura_net_farmed_apy) * (1 + pool_apy)) - 1
-
-    bal_net_apr = (bal_apr_minus_keep_bal + pool_rewards_apr) * (1 - performance_fee) - management_fee
-    bal_net_farmed_apy = (1 + (bal_net_apr / COMPOUNDING)) ** COMPOUNDING - 1
-    bal_net_apy = ((1 + bal_net_farmed_apy) * (1 + pool_apy)) - 1
-
-    net_apy = bal_net_apy * bal_debt_ratio + aura_net_apy * aura_apy_data.debt_ratio
-  
-    boost = voter_boost * bal_debt_ratio
-
-    # 0.3.5+ should never be < 0% because of management
-    if net_apy < 0 and Version(vault.api_version) >= Version('0.3.5'): 
-        net_apy = 0
-
-    fees = ApyFees(
-        performance=performance_fee,
-        management=management_fee,
-        keep_bal=keep_bal,
-        aura_keep_bal=aura_apy_data.keep
-    )
-
-    composite = {
-        "boost": boost,
-        "pool_apy": pool_apy,
-        "boosted_apr": bal_apr,
-        "base_apr": gauge_base_apr,
-        "aura_apr": aura_apy_data.aura_apr,
-        "rewards_apr": pool_rewards_apr
-    }
-
-    if os.getenv('DEBUG', None):
-        logger.info(pformat(Debug().collect_variables(locals())))
-
-    return Apy('bal', gross_apr, net_apy, fees, composite=composite)
-
-def calculate_simple_aura_apy(
-        vault, gauge, 
-        pool_rewards_apr,
-        pool_price_per_share, 
-        pool_token_price, 
-        gauge_base_apr, 
-        pool_apy,
-        samples
-) -> Apy:
-    now = samples.now
     performance_fee, management_fee, keep_bal = get_vault_fees(vault, block=now)
 
-    apy_data = get_aura_apy_data(
+    apr_data = get_current_aura_apr(
         vault, gauge,
         pool_price_per_share, 
-        pool_token_price, 
-        gauge_base_apr,
-        keep_bal,
+        pool_token_price,
         block=now
     )
 
-    gross_apr = (1 + (apy_data.apr * apy_data.debt_ratio)) * (1 + pool_apy) - 1
+    gross_apr = apr_data.gross_apr * apr_data.debt_ratio
 
-    net_booster_apr = (apy_data.apr_minus_keep + apy_data.reward_apr) * (1 - performance_fee) - management_fee
+    net_booster_apr = apr_data.net_apr * (1 - performance_fee) - management_fee
     net_booster_apy = (1 + (net_booster_apr / COMPOUNDING)) ** COMPOUNDING - 1
-    net_apy = ((1 + net_booster_apy) * (1 + pool_apy)) - 1
-
-    # 0.3.5+ should never be < 0% because of management
-    if net_apy < 0 and Version(vault.api_version) >= Version('0.3.5'): 
-        net_apy = 0
+    net_apy = net_booster_apy
 
     fees = ApyFees(
         performance=performance_fee, 
         management=management_fee, 
         keep_crv=keep_bal, 
-        cvx_keep_crv=apy_data.keep
+        cvx_keep_crv=keep_bal
     )
 
     if os.getenv('DEBUG', None):
         logger.info(pformat(Debug().collect_variables(locals())))
 
     composite = {
-        "boost": apy_data.boost,
-        "pool_apy": pool_apy,
-        "boosted_apr": net_booster_apr,
-        "base_apr": gauge_base_apr,
-        "aura_apr": apy_data.apr,
-        "rewards_apr": pool_rewards_apr
+        "boost": apr_data.boost,
+        "bal_rewards_apr": apr_data.bal_apr,
+        "aura_rewards_apr": apr_data.aura_apr,
+        "extra_rewards_apr": apr_data.extra_rewards_apr,
+        "aura_gross_apr": apr_data.gross_apr,
+        "aura_net_apr": apr_data.net_apr,
+        "booster_net_apr": net_booster_apr,
     }
-    blocks = ApyBlocks(samples.now, samples.week_ago, samples.month_ago, vault.reports[0].block_number)
+
+    blocks = ApyBlocks(
+        samples.now, 
+        samples.week_ago, 
+        samples.month_ago, 
+        vault.reports[0].block_number
+    )
+
     return Apy('aura', gross_apr, net_apy, fees, composite=composite, blocks=blocks)
 
-def get_aura_apy_data(
+def get_current_aura_apr(
         vault, gauge, 
         pool_price_per_share, 
-        pool_token_price, 
-        gauge_base_apr,
-        keep_bal,
+        pool_token_price,
         block=None
-) -> AuraApyData:
+) -> AuraAprData:
+    """Calculate the current APR as opposed to projected APR like we do with CRV-CVX"""
     strategy = vault.strategies[0].strategy
     debt_ratio = get_debt_ratio(vault, strategy)
     booster = contract(addresses[chain.id]['booster'])
     booster_fee = get_booster_fee(booster, block)
-    booster_reward_apr = get_booster_reward_apr(strategy, booster, pool_price_per_share, pool_token_price, block)
     booster_boost = gauge.calculate_boost(MAX_BOOST, addresses[chain.id]['booster_voter'], block)
-    aura_emission_rate = get_aura_emission_rate_converted_to_bal(block)
+    extra_rewards_apr = get_booster_reward_apr(strategy, booster, pool_price_per_share, pool_token_price, block)
 
-    booster_apr = (
-        ((1 - booster_fee) * booster_boost * gauge_base_apr) 
-        * (1 + aura_emission_rate) 
-        + booster_reward_apr
-    )
+    bal_price = magic.get_price(addresses[chain.id]['bal'], block=block)
+    aura_price = magic.get_price(addresses[chain.id]['aura'], block=block)
 
-    booster_apr_minus_keep = (
-        ((1 - booster_fee) * booster_boost * gauge_base_apr) 
-        * ((1 - keep_bal) + aura_emission_rate)
-    )
+    rewards = contract(strategy.rewardsContract())
+    rewards_tvl = pool_token_price * rewards.totalSupply() / 10**rewards.decimals()
+
+    bal_rewards_per_year = (rewards.rewardRate()/10**rewards.decimals()) * SECONDS_PER_YEAR
+    bal_rewards_per_year_usd =  bal_rewards_per_year * bal_price
+    bal_rewards_apr = bal_rewards_per_year_usd / rewards_tvl
+
+    aura_emission_rate = get_aura_emission_rate(block)
+    aura_rewards_per_year = bal_rewards_per_year * aura_emission_rate
+    aura_rewards_per_year_usd = aura_rewards_per_year * aura_price
+    aura_rewards_apr = aura_rewards_per_year_usd / rewards_tvl
+
+    gross_apr = bal_rewards_apr + aura_rewards_apr + extra_rewards_apr
+    net_apr = (bal_rewards_apr + aura_rewards_apr) * (1 - booster_fee) + extra_rewards_apr
 
     if os.getenv('DEBUG', None):
         logger.info(pformat(Debug().collect_variables(locals())))
 
-    return AuraApyData(
+    return AuraAprData(
         booster_boost,
-        booster_apr,
-        booster_apr_minus_keep,
-        keep_bal,
-        debt_ratio,
-        booster_reward_apr
+        bal_rewards_apr,
+        aura_rewards_apr,
+        extra_rewards_apr,
+        gross_apr,
+        net_apr,
+        debt_ratio
     )
 
 def get_vault_fees(vault, block=None):
@@ -248,30 +184,34 @@ def get_vault_fees(vault, block=None):
     
     return performance, management, keep_bal
 
-def get_aura_emission_rate_converted_to_bal(block=None) -> float:
-    """AURA emission rate converted to BAL to ease calculation of total APY."""
-    bal_price = magic.get_price(addresses[chain.id]['bal'], block=block)
+def get_aura_emission_rate(block=None) -> float:
     aura = contract(addresses[chain.id]['aura'])
-    total_cliffs = aura.totalCliffs()
     initial_mint = aura.INIT_MINT_AMOUNT()
-    max_supply = initial_mint + aura.EMISSIONS_MAX_SUPPLY()
-    reduction_per_cliff = aura.reductionPerCliff()
     supply = aura.totalSupply(block_identifier=block)
-    current_cliff = (supply - initial_mint) / reduction_per_cliff
+    max_supply = initial_mint + aura.EMISSIONS_MAX_SUPPLY()
+
     if supply <= max_supply:
+        total_cliffs = aura.totalCliffs()
+        minter_minted = get_aura_minter_minted(block)
+        reduction_per_cliff = aura.reductionPerCliff()
+        current_cliff = (supply - initial_mint - minter_minted) / reduction_per_cliff
         reduction =  2.5 * (total_cliffs - current_cliff) + 700
-        minted = reduction / total_cliffs
-        aura_price = magic.get_price(aura, block=block)
 
         if os.getenv('DEBUG', None):
             logger.info(pformat(Debug().collect_variables(locals())))
 
-        return minted * (aura_price / bal_price)
+        return reduction / total_cliffs
     else:
         if os.getenv('DEBUG', None):
             logger.info(pformat(Debug().collect_variables(locals())))
 
         return 0
+
+def get_aura_minter_minted(block=None) -> float:
+    """According to Aura's docs you should use the minterMinted field when calculating the
+    current aura emission rate. The minterMinted field is private in the contract though!?
+    So get it by storage slot"""
+    return web3.eth.get_storage_at(addresses[chain.id]['aura'], 7, block_identifier=block)
 
 def get_debt_ratio(vault, strategy) -> float:
     return vault.vault.strategies(strategy)[2] / 1e4
