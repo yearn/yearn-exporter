319acc34199f0e6bfdf4bd4430137877eb652171
Dark Ghosty <38810205+DarkGhost7@users.noreply.github.com>
2023-03-15 11:21:40 -0400

    feat: set arbi and opti to weekly apy (#488)
    * fix: apy to weekly for all chains
    
    Use weekly apy calculation instead of monthly for all chains but eth
    
    * fix: simple
    
    * fix: commented msg

--- yearn/apy/v2.py
+++ yearn/apy/v2.py
@@ -78,10 +78,10 @@ def simple(vault, samples: ApySamples) -> Apy:
     inception_apy = calculate_roi(now_point, inception_point)
 
     # use the first non-zero apy, ordered by precedence
-    # For Fantom, prioritize the weekly APY over the monthly. 
-    # The APYs on Fantom vary quickly and harvests are more frequent than on mainnet, 
+    # For everything that isn't ETH, prioritize the weekly APY over the monthly. 
+    # APYs on other networks are vary quickly and harvests are more frequent than on mainnet, 
     # where infrequent harvests mean showing the weekly APY results in misleading spikes
-    if chain.id == Network.Fantom:
+    if chain.id != Network.Mainnet:
         apys = [week_ago_apy, month_ago_apy, inception_apy]
     else:
         apys = [month_ago_apy, week_ago_apy, inception_apy]
@@ -169,10 +169,10 @@ def average(vault, samples: ApySamples) -> Apy:
     # we should look at a vault's harvests, age, etc to determine whether to show new APY or not
 
     # use the first non-zero apy, ordered by precedence
-    # For Fantom, prioritize the weekly APY over the monthly. 
-    # The APYs on Fantom vary quickly and harvests are more frequent than on mainnet, 
+    # For everything that isn't ETH, prioritize the weekly APY over the monthly. 
+    # APYs on other networks are vary quickly and harvests are more frequent than on mainnet, 
     # where infrequent harvests mean showing the weekly APY results in misleading spikes
-    if chain.id == Network.Fantom:
+    if chain.id != Network.Mainnet:
         apys = [week_ago_apy, month_ago_apy]
     else:
         apys = [month_ago_apy, week_ago_apy]
