f2426532fd59c3352532563ac1bf159ec5e6eb3a
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-05-05 17:38:41 +0000

    feat: asyncify veyfi exporter

--- scripts/exporters/veyfi.py
+++ scripts/exporters/veyfi.py
@@ -1,28 +1,48 @@
 import json
 import logging
 from datetime import datetime, timezone
-from time import time
+from typing import List
 
 import sentry_sdk
-from brownie import Contract, chain
-from yearn.multicall2 import fetch_multicall
-from yearn.networks import Network
-from yearn.utils import closest_block_after_timestamp, safe_views
-from yearn.snapshot_range_helper import (start_bidirectional_export, time_tracking)
-from yearn.outputs.victoria.output_helper import _build_item, _post
-from yearn.outputs.victoria import output_duration
+from brownie import chain
+from y import Contract, Network
+from y.datatypes import Block
+from y.time import closest_block_after_timestamp
+
+from yearn.helpers.exporter import Exporter
+from yearn.multicall2 import fetch_multicall_async
+from yearn.outputs.victoria.victoria import _build_item, _post
+from yearn.utils import safe_views
 
 sentry_sdk.set_tag('script','veyfi_exporter')
 
 logger = logging.getLogger('yearn.veyfi_exporter')
 
+start = {
+    Network.Mainnet: datetime(2022, 12, 7, 0, tzinfo=timezone.utc)
+}
+
+def main():
+    Exporter(
+        name = "veyfi",
+        data_query = 'veyfi{network="ETH"}',
+        data_fn = VotingYFI().metrics_for_export,
+        export_fn = _post,
+        start_block = closest_block_after_timestamp(start[chain.id]),
+    ).run()
+
+
+
 VEYFI = {
     Network.Mainnet: '0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5',
 }
 
-
+# TODO: Move this into special module rather than its own exporter
 class VotingYFI:
     def __init__(self):
+        if chain.id not in VEYFI:
+            raise NotImplementedError(f"Only supports {VEYFI.keys()}")
+
         self.veyfi = Contract.from_abi(
             name='VotingYFI',
             address=VEYFI[chain.id],
@@ -30,50 +50,28 @@ class VotingYFI:
         )
         self._views = safe_views(self.veyfi.abi)
 
-    def describe(self, block=None):
+    async def describe(self, block=None):
         # TODO: this currently only fetches totalSupply and supply
         #       will need to parse events & transactions
         try:
-            results = fetch_multicall(
+            results = await fetch_multicall_async(
                 *[[self.veyfi, view] for view in self._views], block=block
             )
             info = dict(zip(self._views, results))
         except ValueError as e:
             info = {}
         return info
-
-
-def main():
-    if chain.id == Network.Mainnet:
-        start = datetime(2022, 12, 7, 0, tzinfo=timezone.utc)
-        data_query = 'veyfi{network="ETH"}'
-    else:
-        raise NotImplementedError("Only Mainnet is supported")
-
-    start_bidirectional_export(start, export_snapshot, data_query)
-
-
-@time_tracking
-def export_snapshot(snapshot, ts):
-    start = time()
-    block = closest_block_after_timestamp(ts, wait_for=True)
-    assert block is not None, "no block after timestamp found"
-    export_veyfi(block, ts)
-    duration = time() - start
-    output_duration.export(duration, 1, "veyfi", ts)
-    logger.info("exported veyfi snapshot %s took %.3fs", snapshot, time() - start)
-
-
-def export_veyfi(block, timestamp):
-    data = VotingYFI().describe(block=block)
-    metrics_to_export = []
-    for key, value in data.items():
-        item = _build_item(
-            "veyfi",
-            ['param'],
-            [key],
-            value,
-            timestamp,
-        )
-        metrics_to_export.append(item)
-    _post(metrics_to_export)
+    
+    async def metrics_for_export(self, block: Block, timestamp: int) -> List:
+        metrics_to_export = []
+        data = await self.describe(block)
+        for key, value in data.items():
+            item = _build_item(
+                "veyfi",
+                ['param'],
+                [key],
+                value,
+                timestamp,
+            )
+            metrics_to_export.append(item)
+        return metrics_to_export
