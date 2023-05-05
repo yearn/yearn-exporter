9c474dab6810a25f7bb48085afbf72b931359450
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-05-05 17:38:41 +0000

    fix: data uery

--- scripts/exporters/vaults.py
+++ scripts/exporters/vaults.py
@@ -20,23 +20,23 @@ if Network(chain.id) == Network.Fantom:
     # end: 2021-04-30 first possible date after the Fantom network upgrade
     start = datetime(2021, 4, 30, tzinfo=timezone.utc)
     # ironbank first product deployed on Fantom
-    data_query = 'ironbank{network=' + Network.label() + '}'
+    data_query = 'ironbank{network="' + Network.label() + '"}'
 elif Network(chain.id) == Network.Gnosis:
     # end: yvUSDC vault January-20-2022 06:10:45 AM +-6 UTC
     start = datetime(2022, 1, 20, tzinfo=timezone.utc)
-    data_query = 'yearn_vault{network=' + Network.label() + '}'
+    data_query = 'yearn_vault{network="' + Network.label() + '"}'
 elif Network(chain.id) == Network.Arbitrum:
     # end: yvUSDC 4.3 Sep-05-2021 09:05:50 PM +UTC
     start = datetime(2021, 9, 5, tzinfo=timezone.utc)
-    data_query = 'yearn_vault{network=' + Network.label() + '}'
+    data_query = 'yearn_vault{network="' + Network.label() + '"}'
 elif Network(chain.id) == Network.Optimism:
     # end: yvDAI Aug-06-2022 10:50:49 PM +UTC block 18111485
     start = datetime(2022, 8, 6, tzinfo=timezone.utc)
-    data_query = 'yearn_vault{network=' + Network.label() + '}'
+    data_query = 'yearn_vault{network="' + Network.label() + '"}'
 elif Network(chain.id) == Network.Mainnet:
     # end: 2020-02-12 first iearn deployment
     start = datetime(2020, 2, 12, 0, 1, tzinfo=timezone.utc)
-    data_query = 'iearn{network=' + Network.label() + '}'
+    data_query = 'iearn{network="' + Network.label() + '"}'
 else:
     raise NotImplementedError()
 
