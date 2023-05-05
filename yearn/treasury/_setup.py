910506da1210c506c9503619934df1b419791681
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-05-05 17:38:40 +0000

    fix: shitcoin

--- yearn/treasury/_setup.py
+++ yearn/treasury/_setup.py
@@ -12,6 +12,7 @@ skip_tokens = {
         "0x01234567bac6fF94d7E4f0EE23119CF848F93245", # ETH Blocks
         "0x437a6B880d4b3Be9ed93BD66D6B7f872fc0f5b5E", # Soda
         "0x9d45DAb69f1309F1F55A7280b1f6a2699ec918E8", # yFamily 2021
+        "0xeaaa790591c646b0436f02f63e8Ca56209FEDE4E", # D-Horse
     }
 }
 
