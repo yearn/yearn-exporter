2c0c044b7729ebffd8b2fcd402078976164c43af
dudesahn <23222916+dudesahn@users.noreply.github.com>
2023-07-07 17:03:16 -0400

    fix: add extra staking rewards apr for velo vaults (#538)
    * fix: add extra staking rewards apr for velo vaults
    
    * fix: add samples to velo

--- yearn/apy/velo.py
+++ yearn/apy/velo.py
@@ -9,7 +9,7 @@ from brownie import ZERO_ADDRESS, chain
 from y import Contract, Network
 from y.time import get_block_timestamp
 
-from yearn.apy.common import SECONDS_PER_YEAR, Apy, ApyFees
+from yearn.apy.common import SECONDS_PER_YEAR, Apy, ApyFees, ApySamples
 from yearn.debug import Debug
 from yearn.prices import magic
 
@@ -28,7 +28,7 @@ def get_staking_pool(underlying: str) -> Optional[Contract]:
         staking_pool = registry.gauges(underlying)
         return None if staking_pool == ZERO_ADDRESS else Contract(staking_pool)
         
-def staking(vault: "Vault", staking_rewards: Contract, block: Optional[int]=None) -> float:
+def staking(vault: "Vault", staking_rewards: Contract, samples: ApySamples, block: Optional[int]=None) -> float:
     end = staking_rewards.periodFinish(block_identifier=block)
     current_time = time() if block is None else get_block_timestamp(block)
     total_supply = staking_rewards.totalSupply(block_identifier=block) if hasattr(staking_rewards, "totalSupply") else 0
@@ -52,6 +52,8 @@ def staking(vault: "Vault", staking_rewards: Contract, block: Optional[int]=None
     
     net_apr = gross_apr * (1 - performance) - management 
     net_apy = (1 + (net_apr / COMPOUNDING)) ** COMPOUNDING - 1
+    from yearn.apy.staking_rewards import get_staking_rewards_apr
+    staking_rewards_apr = get_staking_rewards_apr(vault, samples)
     if os.getenv("DEBUG", None):
         logger.info(pformat(Debug().collect_variables(locals())))
-    return Apy("v2:velo", gross_apr=gross_apr, net_apy=net_apy, fees=fees)
+    return Apy("v2:velo", gross_apr=gross_apr, net_apy=net_apy, fees=fees, staking_rewards_apr=staking_rewards_apr)
