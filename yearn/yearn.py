42b03f0b9148b6a2eb515bd591217d3aa3f2a84c
0xBasically <0xBasic@yearn.finance>
2023-08-29 13:38:06 -0400

    add: yearn.py products

--- yearn/yearn.py
+++ yearn/yearn.py
@@ -57,6 +57,11 @@ class Yearn:
                 "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(exclude_ib_tvl=exclude_ib_tvl),
             }
+        elif chain.id == Network.Base:
+            self.registries = {
+                "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
+                "ib": yearn.ironbank.Registry(exclude_ib_tvl=exclude_ib_tvl),
+            }
         else:
             raise UnsupportedNetwork('yearn is not supported on this network')
 
