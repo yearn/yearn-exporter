87f0ce191f278186c08ea7df5fbd6b58f7cdd4cb
0xBasic <0xBasic@yearn.finance>
2023-09-12 15:27:59 -0400

    feat: add aerodrome fwd-looking apys (#623)
    * add: aero fwd calcs
    
    * add: aero voter
    
    * init: aero
    
    * update vaults for aero
    
    * update velo registry to voter

--- yearn/apy/__init__.py
+++ yearn/apy/__init__.py
@@ -1,4 +1,4 @@
-from yearn.apy import v1, v2, velo
+from yearn.apy import v1, v2, velo, aero
 from yearn.apy.common import (Apy, ApyBlocks, ApyError, ApyFees, ApyPoints,
                               ApySamples, get_samples)
 from yearn.apy.curve import simple as curve
