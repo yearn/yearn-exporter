32607afe6d9f6a230f0b235ced828e67db8bd6b0
0xBasic <0xBasic@yearn.finance>
2023-09-15 11:46:08 -0400

    Curve apy previews (#627)
    * fix: catch non verified err
    
    * feat: override aws_endpoint_url for curve_apy_previews script
    
    ---------
    
    Co-authored-by: BobTheBuidler <bobthebuidlerdefi@gmail.com>
    Co-authored-by: crebsy <121096251+crebsy@users.noreply.github.com>

--- scripts/curve_apy_previews.py
+++ scripts/curve_apy_previews.py
@@ -41,7 +41,16 @@ def _build_data(gauges):
     for name, values in gauges.items():
         m = re.match('^([^\(\)]+) \(.*\)', name)
         gauge_name = m[1]
-        gauge = _extract_gauge(values)
+        try:
+            gauge = _extract_gauge(values)
+        except ContractNotVerified as e:
+            data.append({
+                "gauge_name": gauge_name,
+                "apy": dataclasses.asdict(Apy("error", 0, 0, ApyFees(0, 0), ApyPoints(0, 0, 0), error_reason=str(e))),
+                "updated": int(time()),
+                "block": samples.now,
+            })
+            continue
         if not gauge:
             continue
 
@@ -161,10 +170,13 @@ def _upload(data):
 
 
 def _get_s3():
+    aws_endpoint_url = os.environ.get("AWS_ENDPOINT_URL")
     aws_key = os.environ.get("AWS_ACCESS_KEY")
     aws_secret = os.environ.get("AWS_ACCESS_SECRET")
 
     kwargs = {}
+    if aws_endpoint_url is not None:
+        kwargs["endpoint_url"] = aws_endpoint_url
     if aws_key is not None:
         kwargs["aws_access_key_id"] = aws_key
     if aws_secret is not None:
