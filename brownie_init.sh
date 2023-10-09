742387908dd65313d8ef9cd40838639a65fb511a
crebsy <121096251+crebsy@users.noreply.github.com>
2023-10-09 20:55:51 +0200

    fix: only add base network config if it's missing. (#638)

--- brownie_init.sh
+++ brownie_init.sh
@@ -3,9 +3,13 @@ set -e
 
 BROWNIE_NETWORK=${BROWNIE_NETWORK:-mainnet} # default to Ethereum mainnet
 EXPLORER=${EXPLORER:-$DEFAULT_EXPLORER}
-brownie networks add Base base-main host=https://base.meowrpc.com chainid=8453 explorer=https://api.basescan.org/api || true
 
-# modify the network & add Base network
+# add Base to brownie's network list
+if ! brownie networks list | grep Base > /dev/null; then
+  brownie networks add Base base-main host=https://base.meowrpc.com chainid=8453 explorer=https://api.basescan.org/api || true
+fi
+
+# modify the network
 if [[ ! -z "$WEB3_PROVIDER" ]]; then
   brownie networks modify $BROWNIE_NETWORK host=$WEB3_PROVIDER explorer=$EXPLORER
 fi
