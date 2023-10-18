ee1b3ed0fd4513f8fd7f83eaceb4f22ecfc9ecf0
crebsy <121096251+crebsy@users.noreply.github.com>
2023-10-18 15:50:14 +0200

    fix: remove yeth from global yearn registry.

--- yearn/yearn.py
+++ yearn/yearn.py
@@ -13,7 +13,6 @@ import yearn.ironbank
 import yearn.special
 import yearn.v1.registry
 import yearn.v2.registry
-import yearn.yeth
 from yearn.exceptions import UnsupportedNetwork
 from yearn.ironbank import addresses as ironbank_registries
 from yearn.outputs.victoria.output_helper import (_flatten_dict,
@@ -38,7 +37,6 @@ class Yearn:
                 "v2": yearn.v2.registry.Registry(watch_events_forever=watch_events_forever),
                 "ib": yearn.ironbank.Registry(exclude_ib_tvl=exclude_ib_tvl),
                 "special": yearn.special.Registry(),
-                "yeth": yearn.yeth.Registry(),
             }
         elif chain.id in [Network.Gnosis, Network.Base]:
             self.registries = {
