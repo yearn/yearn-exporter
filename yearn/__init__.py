cb4083aeacf52f4916d1dc9c5f82e1aeba51a2fd
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-09 02:10:08 +0000

    fix: optimism

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,5 +1,6 @@
 
-from brownie import network
+from brownie import network, chain, web3
+from web3.middleware.geth_poa import geth_poa_middleware
 
 from yearn.logs import setup_logging
 from yearn.sentry import setup_sentry
@@ -11,9 +12,15 @@ if network.is_connected():
     # If ypm db is not yet initialized, force eth-util extended version
     import eth_portfolio._db.entities
     
+    from y import Network
     from yearn._setup import (customize_ypricemagic,
                               force_init_problematic_contracts)
     from yearn.middleware.middleware import setup_middleware
+    
+    # needed for base and opti
+    
+    if chain.id == Network.Optimism:
+        web3.middleware_onion.inject(geth_poa_middleware, layer=0)
     
     setup_middleware()
     force_init_problematic_contracts()
