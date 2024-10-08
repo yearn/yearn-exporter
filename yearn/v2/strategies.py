a59c5502d6b3fb2adca1241d321cc968dc86f06f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-08 20:39:24 +0000

    fix: Harvests

--- yearn/v2/strategies.py
+++ yearn/v2/strategies.py
@@ -39,7 +39,7 @@ class Strategy:
         except ValueError:
             self.name = strategy[:10]
         self._views = safe_views(self.strategy.abi)
-        self._events = Harvests(self.strategy)
+        self._events = Harvests(self)
 
     @async_property
     async def unique_name(self):
@@ -76,23 +76,26 @@ class Strategy:
 
 
 class Harvests(ProcessedEvents[int]):
-    def __init__(self, strategy: Contract):
+    def __init__(self, strategy: Strategy):
         topics = [
             [
                 encode_hex(event_abi_to_log_topic(event))
-                for event in strategy.abi
+                for event in strategy.strategy.abi
                 if event["type"] == "event" and event["name"] in STRATEGY_EVENTS
             ]
         ]
-        super().__init__(addresses=[str(strategy)], topics=topics)
+        super().__init__(addresses=[str(strategy.strategy)], topics=topics)
+        self.strategy = strategy
     def _include_event(self, event: _EventItem) -> bool:
         return event.name == "Harvested"
+    def _get_block_for_obj(self, block: int) -> int:
+        return block
     # TODO: work this in somehow:
     #   logger.info("loaded %d harvests %s in %.3fs", len(self._harvests), self.name, time.time() - start)
     def _process_event(self, event: _EventItem) -> int:
         block = event.block_number
-        logger.debug("%s harvested on %d", self.name, block)
-        return 
+        logger.debug("%s harvested on %d", self.strategy.name, block)
+        return block
     
 
 def _unpack_results(views: List[str], results: List[Any], scale: int):
