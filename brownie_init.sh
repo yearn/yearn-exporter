d3b27b5fd485d624c0f53279022383b70f13451f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-09 06:29:29 +0000

    feat: faster builds with poetry

--- brownie_init.sh
+++ brownie_init.sh
@@ -5,11 +5,11 @@ BROWNIE_NETWORK=${BROWNIE_NETWORK:-mainnet} # default to Ethereum mainnet
 EXPLORER=${EXPLORER:-$DEFAULT_EXPLORER}
 
 # add Base to brownie's network list
-if ! brownie networks list | grep Base > /dev/null; then
-  brownie networks add Base base-main host=https://base.meowrpc.com chainid=8453 explorer=https://api.basescan.org/api || true
+if ! poetry run brownie networks list | grep Base > /dev/null; then
+  poetry run brownie networks add Base base-main host=https://base.meowrpc.com chainid=8453 explorer=https://api.basescan.org/api || true
 fi
 
 # modify the network
 if [[ ! -z "$WEB3_PROVIDER" ]]; then
-  brownie networks modify $BROWNIE_NETWORK host=$WEB3_PROVIDER explorer=$EXPLORER
+  poetry run brownie networks modify $BROWNIE_NETWORK host=$WEB3_PROVIDER explorer=$EXPLORER
 fi
