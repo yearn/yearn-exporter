b89256e4651927899b430dfbdd7cedb944652639
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-04-12 22:39:24 +0000

    feat: optimize victoria metrics `has_data`

--- yearn/outputs/victoria/victoria.py
+++ yearn/outputs/victoria/victoria.py
@@ -4,52 +4,61 @@ import logging
 import math
 import os
 from decimal import Decimal
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
-import aiohttp
 import eth_retry
-from brownie import chain
-from y.networks import Network
+from aiohttp import ClientError, ClientSession, ServerDisconnectedError
+from msgspec import Raw, Struct
+from msgspec.json import decode
+from y import Network
+from y.constants import CHAINID
 
 BASE_URL = os.environ.get('VM_URL', 'http://victoria-metrics:8428')
 IMPORT_URL = f'{BASE_URL}/api/v1/import'
-VM_REQUEST_HEADERS = {'Connection': 'close', 'Content-Encoding': 'gzip'}
+VM_HAS_DATA_HEADERS = {'Connection': 'close'}
+VM_POST_HEADERS = {'Connection': 'close', 'Content-Encoding': 'gzip'}
+NETWORK_LABEL = Network.label(CHAINID)
 
 logger = logging.getLogger(__name__)
 
+# These 2 helpers enable us to decode only the relevant
+# data in the json response and discard the rest
+class Data(Struct):
+    result: Tuple[Raw, ...]
+
+class Response(Struct):
+    status: str
+    data: Data
+
 @eth_retry.auto_retry
 async def has_data(ts, data_query):
+    # sourcery skip: use-contextlib-suppress
     # query for a metric which should be present
     url = f'{BASE_URL}/api/v1/query?query={data_query}&time={int(ts)}'
-    headers = {
-        'Connection': 'close',
-    }
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         while True:
             try:
-                response = await session.get(
-                    url = url,
-                    headers = headers
-                )
-                result = await response.json()
-                return result['status'] == 'success' and len(result['data']['result']) > 0
-            except aiohttp.client_exceptions.ServerDisconnectedError:
+                response = await session.get(url = url, headers = VM_HAS_DATA_HEADERS)
+                result = decode(await response.read(), type=Response)
+                return result.status == 'success' and len(result.data.result) > 0
+            except ServerDisconnectedError:
                 pass
 
 def _build_item(metric, label_names, label_values, value, timestamp):
     ts_millis = math.floor(timestamp) * 1000
-    label_names.append("network")
-    label_values.append(Network.label(chain.id))
-    meta = dict(zip(map(_sanitize, label_names), map(str, label_values)))
-    meta["__name__"] = metric
-    return {"metric": meta, "values": [_sanitize(value)], "timestamps": [ts_millis]}
+    return {
+        "metric": dict(
+            zip(map(_sanitize_name, label_names), map(str, label_values)), 
+            network=NETWORK_LABEL, 
+            __name__=metric,
+        ), 
+        "values": [_sanitize(value)], 
+        "timestamps": [ts_millis],
+    }
 
 
 def _to_jsonl_gz(metrics_to_export: List[Dict]):
-    lines = []
-    for item in metrics_to_export:
-        lines.append(json.dumps(item))
-
+    lines = map(json.dumps, metrics_to_export)
     jsonlines = "\n".join(lines)
     return gzip.compress(bytes(jsonlines, "utf-8"))
 
@@ -60,15 +69,15 @@ async def _post(metrics_to_export: List[Dict]) -> None:
     attempts = 0
     while True:
         try:
-            async with aiohttp.ClientSession() as session:
+            async with ClientSession() as session:
                 await session.post(
                     url = IMPORT_URL,
-                    headers = VM_REQUEST_HEADERS,
+                    headers = VM_POST_HEADERS,
                     data = data,
                 )
             return
         except Exception as e:
-            if not isinstance(e, aiohttp.ClientError):
+            if not isinstance(e, ClientError):
                 raise e
             attempts += 1
             logger.debug('You had a ClientError: %s', e)
@@ -84,3 +93,12 @@ def _sanitize(value):
     elif isinstance(value, str):
         return value.replace('"', '')  # e.g. '"yvrenBTC" 0.3.5 0x340832'
     return value
+
+
+__sanitized_names = {}
+
+def _sanitize_name(name: str) -> str:
+    sanitized = __sanitized_names.get(name)
+    if sanitized is None:
+        sanitized = __sanitized_names[name] = _sanitize(name)
+    return sanitized
